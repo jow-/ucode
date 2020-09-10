jow-/ucode

commit c735882bb492ff81f98773186652dbe878ff3d60

parser, eval: use an ut_op flag to denote postfix access
The current code still abused the JSON value pointer to denote postfix
access for certain operations which led to a crash when freeing the
parser state due to an attempt to put a (void *)1 pointer.

Since we do have the ability to set flags on operations since the AST
rework, use this much cleaner approach and avoid the invalid pointer
hackery.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

eval.c
@@ -804,7 +804,7 @@ ut_execute_inc_dec(struct ut_state *state, uint32_t off)
 	ut_putval(ut_setval(scope, key, nval));
 
 	/* postfix inc/dec, return old val */
-	if (op->val)
+	if (op->is_postfix)
 		return val;
 
 	ut_putval(val);
@@ -1292,7 +1292,7 @@ ut_execute_op(struct ut_state *state, uint32_t off)
 
 	case T_LBRACK:
 		/* postfix access */
-		if (op->val) {
+		if (op->is_postfix) {
 			scope = ut_getref_required(state, off, &key);
 			state->ctx = scope;
 

ast.h
@@ -55,6 +55,7 @@ struct ut_op {
 	uint16_t is_first:1;
 	uint16_t is_op:1;
 	uint16_t is_overflow:1;
+	uint16_t is_postfix:1;
 	uint32_t off;
 	struct json_object *val;
 	union {

parser.y
@@ -254,14 +254,14 @@ unary_exp(A) ::= T_COMPL(B) unary_exp(C).				{ A = wrap_op(B, C); }
 unary_exp(A) ::= T_NOT(B) unary_exp(C).					{ A = wrap_op(B, C); }
 unary_exp(A) ::= postfix_exp(B).						{ A = B; }
 
-postfix_exp(A) ::= unary_exp(B) T_INC(C).				{ A = wrap_op(C, B); ut_get_op(s, A)->val = (void *)1; }
-postfix_exp(A) ::= unary_exp(B) T_DEC(C).				{ A = wrap_op(C, B); ut_get_op(s, A)->val = (void *)1; }
+postfix_exp(A) ::= unary_exp(B) T_INC(C).				{ A = wrap_op(C, B); ut_get_op(s, A)->is_postfix = 1; }
+postfix_exp(A) ::= unary_exp(B) T_DEC(C).				{ A = wrap_op(C, B); ut_get_op(s, A)->is_postfix = 1; }
 postfix_exp(A) ::= unary_exp(B) T_LPAREN(C) T_RPAREN.	{ A = wrap_op(C, B); }
 postfix_exp(A) ::= unary_exp(B) T_LPAREN(C) arg_exp(D) T_RPAREN.
 														{ A = wrap_op(C, B, D); }
 postfix_exp(A) ::= postfix_exp(B) T_DOT(C) T_LABEL(D).	{ A = wrap_op(C, B, D); }
 postfix_exp(A) ::= postfix_exp(B) T_LBRACK(C) assign_exp(D) T_RBRACK.
-														{ A = wrap_op(C, B, D); ut_get_op(s, A)->val = (void *)1; }
+														{ A = wrap_op(C, B, D); ut_get_op(s, A)->is_postfix = 1; }
 postfix_exp(A) ::= primary_exp(B).						{ A = B; }
 
 primary_exp(A) ::= T_BOOL(B).							{ A = B; }

commit 05b5b677afa6af3026da2c49222ac8358992392d

eval: fix [] subscript operator
Fixes: c735882 ("parser, eval: use an ut_op flag to denote postfix access")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

eval.c
@@ -245,7 +245,7 @@ ut_ref_to_str(struct ut_state *state, uint32_t off)
 		return p;
 
 	case T_LBRACK:
-		if (!op->val)
+		if (!op->is_postfix)
 			return NULL;
 
 		s = ut_ref_to_str(state, op->tree.operand[0]);
@@ -286,7 +286,7 @@ ut_getref(struct ut_state *state, uint32_t off, struct json_object **key)
 
 		return ut_execute_op(state, off1);
 	}
-	else if (op && op->type == T_LBRACK && op->val) {
+	else if (op && op->type == T_LBRACK && op->is_postfix) {
 		*key = off2 ? ut_execute_op(state, off2) : NULL;
 
 		return ut_execute_op(state, off1);