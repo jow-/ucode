2b59097c3f61fa901e91ac4cea48940760439578
Petr Štetiar <ynezz@true.cz>
2021-03-19 16:54:55 +0100

    tests: create custom tests from current tests cases
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- tests/custom/run_tests.sh
+++ tests/custom/run_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 line='........................................'
+ucode_bin=${UCODE_BIN:-./ucode}
 
 extract_sections() {
 	local file=$1
@@ -47,7 +48,7 @@ run_testcase() {
 	local code=$6
 	local fail=0
 
-	./ucode -e '{ "REQUIRE_SEARCH_PATH": [ "./lib/*.so" ] }' -i - <"$in" >"$dir/res.out" 2>"$dir/res.err"
+	$ucode_bin -e '{ "REQUIRE_SEARCH_PATH": [ "./lib/*.so" ] }' -i - <"$in" >"$dir/res.out" 2>"$dir/res.err"
 
 	touch "$dir/empty"
 	printf "%d\n" $? > "$dir/res.code"
@@ -162,7 +163,7 @@ use_test() {
 	return 1
 }
 
-for catdir in tests/[0-9][0-9]_*; do
+for catdir in [0-9][0-9]_*; do
 	[ -d "$catdir" ] || continue
 
 	printf "\n##\n## Running %s tests\n##\n\n" "${catdir##*/[0-9][0-9]_}"
