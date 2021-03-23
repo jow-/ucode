jow-/ucode

commit 9ad9afb24d90cee643c35e254de0b1c734e44940

compiler: fix try/catch miscompilation
When skipping catch blocks with exception variables, jump beyond the
instruction popping the exception variable off the stack to fix a
stack position mismatch between compiler and vm.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

compiler.c
@@ -2485,9 +2485,9 @@ uc_compiler_compile_try(uc_compiler *compiler)
 
 	uc_compiler_parse_consume(compiler, TK_RBRACE);
 
-	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
-
 	uc_compiler_leave_scope(compiler);
+
+	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
 }
 
 static void

tests/03_bugs/01_try_catch_stack_mismatch
@@ -1,0 +1,28 @@
+When compiling a try/catch statement with an exception variable, the catch
+skip jump incorrectly pointed to the POP instruction popping the exception
+variable off the stack, leading to a stack position mismatch between
+compiler and vm, causing local variables to yield wrong values at runtime.
+
+-- Expect stdout --
+1
+-- End --
+
+-- Testcase --
+{%
+	function f() {
+		let x;
+
+		try {
+			x = 1;
+		}
+		catch(e) {
+
+		}
+
+		// Before the fix, `x` incorrectly yielded the print function value
+		print(x, "\n");
+	}
+
+	f()
+%}
+-- End --

commit 091ae1b198e019430f342ae1444c68d6517e077e

compiler: fix another try/catch miscompilation
When skipping over the catch block of a try/catch statement, make sure to
emit the jump after the try scope variables have been popped off the stack
in order to prevent a stack position mismatch between compiler and vm.

Fixes: 9ad9afb ("compiler: fix try/catch miscompilation")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

compiler.c
@@ -2427,13 +2427,13 @@ uc_compiler_compile_try(uc_compiler *compiler)
 	       !uc_compiler_parse_check(compiler, TK_EOF))
 		uc_compiler_compile_declaration(compiler);
 
-	/* jump beyond catch branch */
-	try_to = chunk->count;
-	jmp_off = uc_compiler_emit_jmp(compiler, 0, 0);
-
 	uc_compiler_parse_consume(compiler, TK_RBRACE);
 
 	uc_compiler_leave_scope(compiler);
+
+	/* jump beyond catch branch */
+	try_to = chunk->count;
+	jmp_off = uc_compiler_emit_jmp(compiler, 0, 0);
 
 
 	/* Catch block ---------------------------------------------------------- */

tests/03_bugs/01_try_catch_stack_mismatch
@@ -26,3 +26,27 @@ compiler and vm, causing local variables to yield wrong values at runtime.
 	f()
 %}
 -- End --
+
+
+When compiling a try/catch statement with local variable declearations
+within the try block, the catch skip jump incorrectly happened before the
+local try block variables were popped off the stack, leading to a stack
+position mismatch between compiler and vm, causing local variables to
+yield wrong values at runtime.
+
+-- Expect stdout --
+1
+-- End --
+
+-- Testcase --
+{%
+	try {
+		let a;
+	}
+	catch {}
+
+	let b = 1;
+
+	print(b, "\n");
+%}
+-- End --