jow-/ucode

commit a4785102dc58ce9c8facb716f18d3aaad0538cba

object: prevent registering the same ressource type multiple times
When a module registering custom ressource types, such as "fs.so", is
required multiple times we need to ensure that only one instance of a
given ressource type is registered, otherwise objects created after
subsequent requires will cease to function since the internal type
prototype mismatches.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

object.c
@@ -357,6 +357,13 @@ static uc_ressource_types res_types;
 uc_ressource_type *
 uc_ressource_type_add(const char *name, uc_prototype *proto, void (*freefn)(void *))
 {
+	uc_ressource_type *existing;
+
+	existing = uc_ressource_type_lookup(name);
+
+	if (existing)
+		return existing;
+
 	uc_vector_grow(&res_types);
 
 	res_types.entries[res_types.count].name = name;

tests/03_bugs/05_duplicate_ressource_type
@@ -1,0 +1,31 @@
+When requiring a C module that registers custom ressource types multiple
+times, ressource values instantiated after subsequent requires of the
+same extensions didn't properly function since the internal type prototype
+was resolved to the initial copy and subsequently discarded due to an index
+mismatch.
+
+-- Testcase --
+{%
+	fs = require("fs");
+	fd = fs.open("README.md");
+
+	printf("fd.read() #1: %s\n",
+		fd.read("line") ? "working" : "not working (" + fd.error() + ")");
+
+	fd.close();
+
+
+	fs = require("fs");
+	fd = fs.open("README.md");
+
+	printf("fd.read() #2: %s\n",
+		fd.read("line") ? "working" : "not working (" + fd.error() + ")");
+
+	fd.close();
+%}
+-- End --
+
+-- Expect stdout --
+fd.read() #1: working
+fd.read() #2: working
+-- End --

commit 03f132427bffa96099611897c7c8ade279639874

object: free prototype object when registering existing ressource type
Fixes: a478510 ("object: prevent registering the same ressource type multiple times")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

object.c
@@ -361,8 +361,11 @@ uc_ressource_type_add(const char *name, uc_prototype *proto, void (*freefn)(void
 
 	existing = uc_ressource_type_lookup(name);
 
-	if (existing)
+	if (existing) {
+		uc_value_put(proto->header.jso);
+
 		return existing;
+	}
 
 	uc_vector_grow(&res_types);
 