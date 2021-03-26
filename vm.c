jow-/ucode

commit 20a3763d0a9a5efe6478595fe5590d47bffa90fe

vm: fix loop variable memory leak in NEXTK/NEXTKV instruction
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

vm.c
@@ -1749,6 +1749,7 @@ uc_vm_insn_next(uc_vm *vm, enum insn_type insn)
 				uc_vm_stack_push(vm, uc_value_get((json_object *)curr->v));
 
 			uc_vm_stack_push(vm, k);
+			uc_value_put(v);
 
 			return;
 		}
@@ -1770,9 +1771,12 @@ uc_vm_insn_next(uc_vm *vm, enum insn_type insn)
 			uc_vm_stack_push(vm, uc_value_get(json_object_array_get_idx(v, n)));
 
 			uc_vm_stack_push(vm, k);
+			uc_value_put(v);
 
 			return;
 		}
+
+		uc_value_put(k);
 
 		break;
 
@@ -1785,6 +1789,8 @@ uc_vm_insn_next(uc_vm *vm, enum insn_type insn)
 
 	if (insn == I_NEXTKV)
 		uc_vm_stack_push(vm, NULL);
+
+	uc_value_put(v);
 }
 
 static void

commit 0341d64e2bd9aa67767d489f8fa3b9d34d61847a

vm: fix another for-loop memory leak
Fixes: 20a3763 ("vm: fix loop variable memory leak in NEXTK/NEXTKV instruction")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

vm.c
@@ -1783,8 +1783,6 @@ uc_vm_insn_next(uc_vm *vm, enum insn_type insn)
 			return;
 		}
 
-		uc_value_put(k);
-
 		break;
 
 	default:
@@ -1797,6 +1795,7 @@ uc_vm_insn_next(uc_vm *vm, enum insn_type insn)
 	if (insn == I_NEXTKV)
 		uc_vm_stack_push(vm, NULL);
 
+	uc_value_put(k);
 	uc_value_put(v);
 }
 