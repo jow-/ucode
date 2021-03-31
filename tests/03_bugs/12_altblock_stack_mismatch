jow-/ucode

commit 97bf297aebaca893734edc5d90a225479494c218

compiler: ensure that alternative if/for/while syntax has own block scope
The `if ...: endif`, `for ...: endfor`, `while ...: endwhile` etc. syntax
statements are supposed to have their own lexical scope, like curly brace
blocks in normal statements.

Without this, local variable declarations within such blocks would
incorrectly shift stack offsets for the remainder of the program.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

compiler.c
@@ -1867,12 +1867,16 @@ uc_compiler_compile_local(uc_compiler *compiler)
 static uc_tokentype_t
 uc_compiler_compile_altifblock(uc_compiler *compiler)
 {
+	uc_compiler_enter_scope(compiler);
+
 	while (true) {
 		switch (compiler->parser->curr.type) {
 		case TK_ELIF:
 		case TK_ELSE:
 		case TK_ENDIF:
 		case TK_EOF:
+			uc_compiler_leave_scope(compiler);
+
 			return compiler->parser->curr.type;
 
 		default:

tests/03_bugs/12_altblock_stack_mismatch
@@ -1,0 +1,26 @@
+When compiling alternative syntax blocks, such as `for ...: endfor`,
+`if ...: endif` etc., the compiler didn't assign the contained statements
+to a dedicated lexical scope, which caused a stack mismatch between
+compiler and vm when such blocks declaring local variables weren't
+actually executed.
+
+-- Expect stdout --
+2
+-- End --
+
+-- Testcase --
+{%
+	if (false):
+		let a = 1;
+	endif;
+
+	/* Due to lack of own lexical scope above, the compiler assumed
+	 * that `a` is still on stack but the code to initialize it was
+	 * never executed, so stack offsets were shifted by one from here
+	 * on throughout the rest of the program. */
+
+	let b = 2;
+
+	print(b, "\n");
+%}
+-- End --

commit 973cc6c865dcf6075c493125313058b650b08234

compiler: actually expand block scope fix to for/while alt syntax
Fixes: 97bf297 ("compiler: ensure that alternative if/for/while syntax has own block scope")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

compiler.c
@@ -2016,11 +2016,15 @@ uc_compiler_compile_while(uc_compiler *compiler)
 
 	/* compile loop body */
 	if (uc_compiler_parse_match(compiler, TK_COLON)) {
+		uc_compiler_enter_scope(compiler);
+
 		if (!uc_compiler_compile_delimitted_block(compiler, TK_ENDWHILE))
 			uc_compiler_syntax_error(compiler, compiler->parser->curr.pos,
 				"Expecting 'endwhile'");
 		else
 			uc_compiler_parse_advance(compiler);
+
+		uc_compiler_leave_scope(compiler);
 	}
 	else {
 		uc_compiler_compile_statement(compiler);
@@ -2116,11 +2120,15 @@ uc_compiler_compile_for_in(uc_compiler *compiler, bool local, uc_token *kvar, uc
 
 	/* compile loop body */
 	if (uc_compiler_parse_match(compiler, TK_COLON)) {
+		uc_compiler_enter_scope(compiler);
+
 		if (!uc_compiler_compile_delimitted_block(compiler, TK_ENDFOR))
 			uc_compiler_syntax_error(compiler, compiler->parser->curr.pos,
 				"Expecting 'endfor'");
 		else
 			uc_compiler_parse_advance(compiler);
+
+		uc_compiler_leave_scope(compiler);
 	}
 	else {
 		uc_compiler_compile_statement(compiler);
@@ -2219,11 +2227,15 @@ uc_compiler_compile_for_count(uc_compiler *compiler, bool local, uc_token *var)
 
 	/* Body ----------------------------------------------------------------- */
 	if (uc_compiler_parse_match(compiler, TK_COLON)) {
+		uc_compiler_enter_scope(compiler);
+
 		if (!uc_compiler_compile_delimitted_block(compiler, TK_ENDFOR))
 			uc_compiler_syntax_error(compiler, compiler->parser->curr.pos,
 				"Expecting 'endfor'");
 		else
 			uc_compiler_parse_advance(compiler);
+
+		uc_compiler_leave_scope(compiler);
 	}
 	else {
 		uc_compiler_compile_statement(compiler);

tests/03_bugs/12_altblock_stack_mismatch
@@ -24,3 +24,60 @@ actually executed.
 	print(b, "\n");
 %}
 -- End --
+
+
+Test a variation of the bug using `for in..endfor` loop syntax.
+
+-- Expect stdout --
+2
+-- End --
+
+-- Testcase --
+{%
+	for (let x in []):
+		let a = 1;
+	endfor;
+
+	let b = 2;
+
+	print(b, "\n");
+%}
+-- End --
+
+
+Test a variation of the bug using `for..endfor` count loop syntax.
+
+-- Expect stdout --
+2
+-- End --
+
+-- Testcase --
+{%
+	for (let i = 0; i < 0; i++):
+		let a = 1;
+	endfor;
+
+	let b = 2;
+
+	print(b, "\n");
+%}
+-- End --
+
+
+Test a variation of the bug using `while..endwhile` loop syntax.
+
+-- Expect stdout --
+2
+-- End --
+
+-- Testcase --
+{%
+	while (false):
+		let a = 1;
+	endwhile;
+
+	let b = 2;
+
+	print(b, "\n");
+%}
+-- End --