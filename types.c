jow-/ucode

commit c79ff390fa4696543057a25ea7bf93c7775652f5

types: handle conversion errors when dealing with negative error indexes
Also apply the same logic to array set operations.

Fixes: 3315b1f ("types: allow negative array indexes")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

types.c
@@ -1948,7 +1948,7 @@ ucv_key_to_index(uc_value_t *val)
 		d = ucv_double_get(val);
 
 		if ((double)(int64_t)(d) != d)
-			return -1;
+			return INT64_MIN;
 
 		return (int64_t)d;
 	}
@@ -1961,12 +1961,12 @@ ucv_key_to_index(uc_value_t *val)
 		idx = strtoll(k, &e, 0);
 
 		if (errno != 0 || e == k || *e != 0)
-			return -1;
+			return INT64_MIN;
 
 		return idx;
 	}
 
-	return -1;
+	return INT64_MIN;
 }
 
 uc_value_t *
@@ -1980,7 +1980,7 @@ ucv_key_get(uc_vm_t *vm, uc_value_t *scope, uc_value_t *key)
 	if (ucv_type(scope) == UC_ARRAY) {
 		idx = ucv_key_to_index(key);
 
-		if (idx < 0 && idx >= -INT64_MAX && llabs(idx) <= ucv_array_length(scope))
+		if (idx < 0 && idx > INT64_MIN && llabs(idx) <= ucv_array_length(scope))
 			idx += ucv_array_length(scope);
 
 		if (idx >= 0 && (uint64_t)idx < ucv_array_length(scope))
@@ -2016,6 +2016,9 @@ ucv_key_set(uc_vm_t *vm, uc_value_t *scope, uc_value_t *key, uc_value_t *val)
 
 	if (ucv_type(scope) == UC_ARRAY) {
 		idx = ucv_key_to_index(key);
+
+		if (idx < 0 && idx > INT64_MIN && llabs(idx) <= ucv_array_length(scope))
+			idx += ucv_array_length(scope);
 
 		if (idx < 0 || !ucv_array_set(scope, idx, val))
 			return NULL;

commit d49af4eb9b111442b4e4fce56a154fb8f27c9b5d

types: fix comparison of differently signed integers
Fixes: c79ff39 ("types: handle conversion errors when dealing with negative error indexes")
Fixes: 3315b1f ("types: allow negative array indexes")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

types.c
@@ -1980,7 +1980,7 @@ ucv_key_get(uc_vm_t *vm, uc_value_t *scope, uc_value_t *key)
 	if (ucv_type(scope) == UC_ARRAY) {
 		idx = ucv_key_to_index(key);
 
-		if (idx < 0 && idx > INT64_MIN && llabs(idx) <= ucv_array_length(scope))
+		if (idx < 0 && idx > INT64_MIN && (uint64_t)llabs(idx) <= ucv_array_length(scope))
 			idx += ucv_array_length(scope);
 
 		if (idx >= 0 && (uint64_t)idx < ucv_array_length(scope))
@@ -2017,7 +2017,7 @@ ucv_key_set(uc_vm_t *vm, uc_value_t *scope, uc_value_t *key, uc_value_t *val)
 	if (ucv_type(scope) == UC_ARRAY) {
 		idx = ucv_key_to_index(key);
 
-		if (idx < 0 && idx > INT64_MIN && llabs(idx) <= ucv_array_length(scope))
+		if (idx < 0 && idx > INT64_MIN && (uint64_t)llabs(idx) <= ucv_array_length(scope))
 			idx += ucv_array_length(scope);
 
 		if (idx < 0 || !ucv_array_set(scope, idx, val))