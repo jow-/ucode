jow-/ucode

commit 9041e2403d98fdb54206c23bd684a7da6fb63026

lib: fix uninitialized memory access on handling %J string formats
When parsing the padding size specification of a `J` format, e.g. `%.4J`,
the internally called `atoi()` function might read beyond the end of the
initialized memory within the format buffer, leading to non-deterministic
results.

Avoid overreading the initialized memory by parsing the padding length
manually digit-by-digit.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

lib.c
@@ -1395,8 +1395,10 @@ uc_printf_common(uc_vm_t *vm, size_t nargs, uc_stringbuf_t *buf)
 
 				for (i = 0; sfmt + i < fp; i++) {
 					if (sfmt[i] == '.') {
-						pad_size = 1 + atoi(&sfmt[i + 1]);
-						fp = &sfmt[i];
+						for (pad_size = 0, i++; sfmt + i < fp && isdigit(sfmt[i]); i++)
+							pad_size = pad_size * 10 + (sfmt[i] - '0');
+						pad_size++;
+						fp = &sfmt[i-1];
 						break;
 					}
 				}

commit 01132db357544327e4934c2b1a5255cd8b2a7c1e

lib: fix %J string formats with precision specifier
Previous refactoring of the code led to an invalid internal format pettern
being used to output the formatted JSON data.

Fixes: 9041e24 ("lib: fix uninitialized memory access on handling %J string formats")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

lib.c
@@ -1324,8 +1324,9 @@ uc_printf_common(uc_vm_t *vm, size_t nargs, uc_stringbuf_t *buf)
 					if (sfmt[i] == '.') {
 						for (pad_size = 0, i++; sfmt + i < fp && isdigit(sfmt[i]); i++)
 							pad_size = pad_size * 10 + (sfmt[i] - '0');
+
 						pad_size++;
-						fp = &sfmt[i-1];
+						fp = strchr(sfmt, '.');
 						break;
 					}
 				}