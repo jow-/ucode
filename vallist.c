jow-/ucode

commit b605dbfcf04f310e08634b52507da7a4155bfce1

treewide: rework numeric value handling
 - Parse integer literals as unsigned numeric values in order to be able
   to represent the entire unsigned 64bit value range

 - Stop parsing minus-prefixed integer literals as negative numbers but
   treat them as separate minus operator followed by a positive integer
   instead

 - Only store unsigned numeric constants in bytecode

 - Rework numeric comparison logic to be able to handle full 64bit
   unsigned integers

 - If possible, yield unsigned 64 bit results for additions

 - Simplify numeric value conversion API

 - Compile code with -fwrapv for defined signed overflow semantics

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

vallist.c
@@ -19,6 +19,7 @@
 #include <math.h> /* isnan(), INFINITY */
 #include <ctype.h> /* isspace(), isdigit(), isxdigit() */
 #include <errno.h>
+#include <assert.h>
 
 #include "ucode/util.h"
 #include "ucode/chunk.h"
@@ -28,12 +29,11 @@
 #define TAG_TYPE			uint64_t
 #define TAG_BITS			3
 #define TAG_MASK			((1LL << ((sizeof(TAG_TYPE) << 3) - TAG_BITS)) - 1)
-#define TAG_MAXN			(TAG_MASK / 2)
 #define TAG_ALIGN(s)		(((s) + (1 << TAG_BITS) - 1) & -(1 << TAG_BITS))
 #define TAG_GET_TYPE(n)		(int)((TAG_TYPE)n & ((1 << TAG_BITS) - 1))
-#define TAG_FIT_NV(n)		((int64_t)n >= -TAG_MAXN && (int64_t)n <= TAG_MAXN)
-#define TAG_SET_NV(n)		((TAG_TYPE)((int64_t)n + TAG_MAXN) << TAG_BITS)
-#define TAG_GET_NV(n)		(int64_t)((int64_t)(((TAG_TYPE)n >> TAG_BITS) & TAG_MASK) - TAG_MAXN)
+#define TAG_FIT_NV(n)		((uint64_t)n <= TAG_MASK)
+#define TAG_SET_NV(n)		((TAG_TYPE)(uint64_t)n << TAG_BITS)
+#define TAG_GET_NV(n)		(uint64_t)((uint64_t)((TAG_TYPE)n >> TAG_BITS) & TAG_MASK)
 #define TAG_FIT_STR(l)		((l - 1) < (((sizeof(TAG_TYPE) << 3) - TAG_BITS) >> 3))
 #define TAG_SET_STR_L(l)	(TAG_TYPE)((l & ((1 << (8 - TAG_BITS)) - 1)) << TAG_BITS)
 #define TAG_GET_STR_L(n)	(size_t)(((TAG_TYPE)n >> TAG_BITS) & ((1 << (8 - TAG_BITS)) - 1))
@@ -42,6 +42,56 @@
 
 #define UC_VALLIST_CHUNK_SIZE	8
 
+
+uc_value_t *
+uc_number_parse(const char *buf, char **end)
+{
+	unsigned long long u;
+	const char *p = buf;
+	bool neg = false;
+	double d;
+	char *e;
+
+	while (isspace(*p))
+		p++;
+
+	if (*p == '-') {
+		neg = true;
+		p++;
+	}
+
+	if (!isxdigit(*p))
+		return NULL;
+
+	if (!end)
+		end = &e;
+
+	u = strtoull(p, end, 0);
+
+	if (**end == '.' || **end == 'e' || **end == 'E') {
+		d = strtod(p, end);
+
+		if (*end == p || (!isspace(**end) && **end != 0))
+			return NULL;
+
+		if (neg)
+			d = -d;
+
+		return ucv_double_new(d);
+	}
+
+	if (*end == p || (!isspace(**end) && **end != 0))
+		return NULL;
+
+	if (neg) {
+		if (u > INT64_MAX)
+			return ucv_int64_new(INT64_MIN);
+
+		return ucv_int64_new(-(int64_t)u);
+	}
+
+	return ucv_uint64_new(u);
+}
 
 void
 uc_vallist_init(uc_value_list_t *list)
@@ -72,7 +122,7 @@ uc_vallist_free(uc_value_list_t *list)
 }
 
 static void
-add_num(uc_value_list_t *list, int64_t n)
+add_num(uc_value_list_t *list, uint64_t n)
 {
 	size_t sz = TAG_ALIGN(sizeof(n));
 
@@ -97,7 +147,7 @@ add_num(uc_value_list_t *list, int64_t n)
 }
 
 static ssize_t
-find_num(uc_value_list_t *list, int64_t n)
+find_num(uc_value_list_t *list, uint64_t n)
 {
 	TAG_TYPE search;
 	size_t i;
@@ -114,10 +164,10 @@ find_num(uc_value_list_t *list, int64_t n)
 			if (TAG_GET_TYPE(list->index[i]) != TAG_LNUM)
 				continue;
 
-			if (TAG_GET_OFFSET(list->index[i]) + sizeof(int64_t) > list->dsize)
+			if (TAG_GET_OFFSET(list->index[i]) + sizeof(uint64_t) > list->dsize)
 				continue;
 
-			if ((int64_t)be64toh(*(int64_t *)(list->data + TAG_GET_OFFSET(list->index[i]))) != n)
+			if ((uint64_t)be64toh(*(uint64_t *)(list->data + TAG_GET_OFFSET(list->index[i]))) != n)
 				continue;
 
 			return i;
@@ -278,6 +328,7 @@ ssize_t
 uc_vallist_add(uc_value_list_t *list, uc_value_t *value)
 {
 	ssize_t existing;
+	uint64_t u64;
 
 	if ((list->isize % UC_VALLIST_CHUNK_SIZE) == 0) {
 		list->index = xrealloc(list->index, sizeof(list->index[0]) * (list->isize + UC_VALLIST_CHUNK_SIZE));
@@ -286,13 +337,16 @@ uc_vallist_add(uc_value_list_t *list, uc_value_t *value)
 
 	switch (ucv_type(value)) {
 	case UC_INTEGER:
-		/* XXX: u64 */
-		existing = find_num(list, ucv_int64_get(value));
+		u64 = ucv_uint64_get(value);
+
+		assert(errno == 0);
+
+		existing = find_num(list, u64);
 
 		if (existing > -1)
 			return existing;
 
-		add_num(list, ucv_int64_get(value));
+		add_num(list, u64);
 
 		break;
 
@@ -348,14 +402,13 @@ uc_vallist_get(uc_value_list_t *list, size_t idx)
 
 	switch (uc_vallist_type(list, idx)) {
 	case TAG_NUM:
-		return ucv_int64_new(TAG_GET_NV(list->index[idx]));
+		return ucv_uint64_new(TAG_GET_NV(list->index[idx]));
 
 	case TAG_LNUM:
-		if (TAG_GET_OFFSET(list->index[idx]) + sizeof(int64_t) > list->dsize)
+		if (TAG_GET_OFFSET(list->index[idx]) + sizeof(uint64_t) > list->dsize)
 			return NULL;
 
-		/* XXX: u64 */
-		return ucv_int64_new(be64toh(*(int64_t *)(list->data + TAG_GET_OFFSET(list->index[idx]))));
+		return ucv_uint64_new(be64toh(*(uint64_t *)(list->data + TAG_GET_OFFSET(list->index[idx]))));
 
 	case TAG_DBL:
 		if (TAG_GET_OFFSET(list->index[idx]) + sizeof(double) > list->dsize)

CMakeLists.txt
@@ -3,7 +3,7 @@ include(CheckFunctionExists)
 include(CheckSymbolExists)
 
 PROJECT(ucode C)
-ADD_DEFINITIONS(-Os -Wall -Werror --std=gnu99 -ffunction-sections -D_GNU_SOURCE)
+ADD_DEFINITIONS(-Os -Wall -Werror --std=gnu99 -ffunction-sections -fwrapv -D_GNU_SOURCE)
 
 IF(CMAKE_C_COMPILER_VERSION VERSION_GREATER 6)
 	ADD_DEFINITIONS(-Wextra -Werror=implicit-function-declaration)
@@ -64,6 +64,11 @@ TARGET_LINK_LIBRARIES(ucode libucode ${json})
 CHECK_FUNCTION_EXISTS(dlopen DLOPEN_FUNCTION_EXISTS)
 IF (NOT DLOPEN_FUNCTION_EXISTS)
   TARGET_LINK_LIBRARIES(libucode dl)
+ENDIF()
+
+CHECK_FUNCTION_EXISTS(fmod FMOD_FUNCTION_EXISTS)
+IF (NOT FMOD_FUNCTION_EXISTS)
+  TARGET_LINK_LIBRARIES(libucode m)
 ENDIF()
 
 SET(CMAKE_REQUIRED_LIBRARIES json-c)

compiler.c
@@ -15,6 +15,7 @@
  */
 
 #include <assert.h>
+#include <errno.h>
 
 #include "ucode/compiler.h"
 #include "ucode/chunk.h"
@@ -410,15 +411,6 @@ uc_compiler_emit_u8(uc_compiler_t *compiler, size_t srcpos, uint8_t n)
 }
 
 static size_t
-uc_compiler_emit_s8(uc_compiler_t *compiler, size_t srcpos, int8_t n)
-{
-	return uc_chunk_add(
-		uc_compiler_current_chunk(compiler),
-		n + 0x7f,
-		uc_compiler_set_srcpos(compiler, srcpos));
-}
-
-static size_t
 uc_compiler_emit_u16(uc_compiler_t *compiler, size_t srcpos, uint16_t n)
 {
 	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
@@ -426,19 +418,6 @@ uc_compiler_emit_u16(uc_compiler_t *compiler, size_t srcpos, uint16_t n)
 
 	uc_chunk_add(chunk, n / 0x100, lineoff);
 	uc_chunk_add(chunk, n % 0x100, 0);
-
-	return chunk->count - 2;
-}
-
-static size_t
-uc_compiler_emit_s16(uc_compiler_t *compiler, size_t srcpos, int16_t n)
-{
-	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
-	size_t lineoff = uc_compiler_set_srcpos(compiler, srcpos);
-	uint16_t v = n + 0x7fff;
-
-	uc_chunk_add(chunk, v / 0x100, lineoff);
-	uc_chunk_add(chunk, v % 0x100, 0);
 
 	return chunk->count - 2;
 }
@@ -866,6 +845,7 @@ uc_compiler_emit_inc_dec(uc_compiler_t *compiler, uc_tokentype_t toktype, bool i
 	uc_value_t *varname = NULL;
 	uc_vm_insn_t type;
 	uint32_t cidx = 0;
+	int insn;
 
 	/* determine kind of emitted load instruction and operand value (if any) */
 	type = chunk->entries ? chunk->entries[compiler->last_insn] : 0;
@@ -901,30 +881,32 @@ uc_compiler_emit_inc_dec(uc_compiler_t *compiler, uc_tokentype_t toktype, bool i
 		return;
 	}
 
+	insn = (toktype == TK_INC) ? I_PLUS : I_MINUS;
+
 	/* add / substract 1 */
 	uc_compiler_emit_insn(compiler, 0, I_LOAD8);
-	uc_compiler_emit_s8(compiler, 0, (toktype == TK_INC) ? 1 : -1);
+	uc_compiler_emit_u8(compiler, 0, 1);
 
 	/* depending on variable type, emit corresponding increment instruction */
 	switch (type) {
 	case I_LVAR:
 		uc_compiler_emit_insn(compiler, 0, I_UVAR);
-		uc_compiler_emit_u32(compiler, 0, (I_PLUS << 24) | cidx);
+		uc_compiler_emit_u32(compiler, 0, (insn << 24) | cidx);
 		break;
 
 	case I_LLOC:
 		uc_compiler_emit_insn(compiler, 0, I_ULOC);
-		uc_compiler_emit_u32(compiler, 0, (I_PLUS << 24) | cidx);
+		uc_compiler_emit_u32(compiler, 0, (insn << 24) | cidx);
 		break;
 
 	case I_LUPV:
 		uc_compiler_emit_insn(compiler, 0, I_UUPV);
-		uc_compiler_emit_u32(compiler, 0, (I_PLUS << 24) | cidx);
+		uc_compiler_emit_u32(compiler, 0, (insn << 24) | cidx);
 		break;
 
 	case I_LVAL:
 		uc_compiler_emit_insn(compiler, 0, I_UVAL);
-		uc_compiler_emit_u8(compiler, 0, I_PLUS);
+		uc_compiler_emit_u8(compiler, 0, insn);
 		break;
 
 	default:
@@ -934,7 +916,7 @@ uc_compiler_emit_inc_dec(uc_compiler_t *compiler, uc_tokentype_t toktype, bool i
 	/* for post increment or decrement, add/substract 1 to yield final value */
 	if (is_postfix) {
 		uc_compiler_emit_insn(compiler, 0, I_LOAD8);
-		uc_compiler_emit_s8(compiler, 0, 1);
+		uc_compiler_emit_u8(compiler, 0, 1);
 
 		uc_compiler_emit_insn(compiler, 0, (toktype == TK_INC) ? I_SUB : I_ADD);
 	}
@@ -1466,7 +1448,7 @@ static void
 uc_compiler_compile_constant(uc_compiler_t *compiler)
 {
 	uc_function_t *fn;
-	int64_t n;
+	uint64_t u;
 
 	switch (compiler->parser->prev.type) {
 	case TK_THIS:
@@ -1502,19 +1484,20 @@ uc_compiler_compile_constant(uc_compiler_t *compiler)
 		break;
 
 	case TK_NUMBER:
-		n = ucv_int64_get(compiler->parser->prev.uv);
+		u = ucv_uint64_get(compiler->parser->prev.uv);
+		assert(errno == 0);
 
-		if (n >= -0x7f && n <= 0x7f) {
+		if (u <= 0xff) {
 			uc_compiler_emit_insn(compiler, compiler->parser->prev.pos, I_LOAD8);
-			uc_compiler_emit_s8(compiler, compiler->parser->prev.pos, n);
+			uc_compiler_emit_u8(compiler, compiler->parser->prev.pos, u);
 		}
-		else if (n >= -0x7fff && n <= 0x7fff) {
+		else if (u <= 0xffff) {
 			uc_compiler_emit_insn(compiler, compiler->parser->prev.pos, I_LOAD16);
-			uc_compiler_emit_s16(compiler, compiler->parser->prev.pos, n);
+			uc_compiler_emit_u16(compiler, compiler->parser->prev.pos, u);
 		}
-		else if (n >= -0x7fffffff && n <= 0x7fffffff) {
+		else if (u <= 0xffffffff) {
 			uc_compiler_emit_insn(compiler, compiler->parser->prev.pos, I_LOAD32);
-			uc_compiler_emit_s32(compiler, compiler->parser->prev.pos, n);
+			uc_compiler_emit_u32(compiler, compiler->parser->prev.pos, u);
 		}
 		else {
 			uc_compiler_emit_constant(compiler, compiler->parser->prev.pos, compiler->parser->prev.uv);

include/ucode/types.h
@@ -372,24 +372,46 @@ void ucv_to_stringbuf_formatted(uc_vm_t *, uc_stringbuf_t *, uc_value_t *, size_
 
 uc_type_t ucv_cast_number(uc_value_t *, int64_t *, double *);
 
+uc_value_t *ucv_to_number(uc_value_t *);
+
 static inline double
 ucv_to_double(uc_value_t *v)
 {
-	int64_t n;
+	uc_value_t *nv;
 	double d;
 
-	return (ucv_cast_number(v, &n, &d) == UC_DOUBLE) ? d : (double)n;
+	nv = ucv_to_number(v);
+	d = ucv_double_get(nv);
+	ucv_put(nv);
+
+	return d;
 }
 
 static inline int64_t
 ucv_to_integer(uc_value_t *v)
 {
+	uc_value_t *nv;
 	int64_t n;
-	double d;
 
-	return (ucv_cast_number(v, &n, &d) == UC_DOUBLE) ? (int64_t)d : n;
+	nv = ucv_to_number(v);
+	n = ucv_int64_get(nv);
+	ucv_put(nv);
+
+	return n;
 }
 
+static inline uint64_t
+ucv_to_unsigned(uc_value_t *v)
+{
+	uc_value_t *nv;
+	uint64_t u;
+
+	nv = ucv_to_number(v);
+	u = ucv_uint64_get(nv);
+	ucv_put(nv);
+
+	return u;
+}
 
 static inline bool
 ucv_is_callable(uc_value_t *uv)
@@ -437,7 +459,7 @@ ucv_is_scalar(uc_value_t *uv)
 bool ucv_is_equal(uc_value_t *, uc_value_t *);
 bool ucv_is_truish(uc_value_t *);
 
-bool ucv_compare(int, uc_value_t *, uc_value_t *);
+bool ucv_compare(int, uc_value_t *, uc_value_t *, int *);
 
 uc_value_t *ucv_key_get(uc_vm_t *, uc_value_t *, uc_value_t *);
 uc_value_t *ucv_key_set(uc_vm_t *, uc_value_t *, uc_value_t *, uc_value_t *);

include/ucode/vallist.h
@@ -41,6 +41,8 @@ typedef enum {
 	TAG_PTR = 6
 } uc_value_type_t;
 
+uc_value_t *uc_number_parse(const char *buf, char **end);
+
 void uc_vallist_init(uc_value_list_t *list);
 void uc_vallist_free(uc_value_list_t *list);
 

lexer.c
@@ -121,7 +121,7 @@ static const struct token tokens[] = {
 	{ TK_RPAREN,	{ .pat = ")" },     1, NULL },
 	{ TK_QMARK,		{ .pat = "?" },     1, NULL },
 	{ TK_SCOL,		{ .pat = ";" },     1, NULL },
-	//{ TK_SUB,		{ .pat = "-" },     1, NULL },
+	{ TK_SUB,		{ .pat = "-" },     1, NULL },
 	{ TK_DOT,		{ .pat = "." },     1, NULL },
 	{ TK_STRING,	{ .pat = "'" },     1, parse_string },
 	{ TK_STRING,	{ .pat = "\"" },    1, parse_string },
@@ -129,7 +129,6 @@ static const struct token tokens[] = {
 	{ TK_LABEL,		{ .pat = "_" },     1, parse_label },
 	{ TK_LABEL,		{ .pat = "az" },    0, parse_label },
 	{ TK_LABEL,		{ .pat = "AZ" },    0, parse_label },
-	{ TK_NUMBER,	{ .pat = "-" },     1, parse_number },
 	{ TK_NUMBER,	{ .pat = "09" },    0, parse_number },
 };
 
@@ -779,39 +778,26 @@ is_numeric_char(uc_lexer_t *lex, char c)
 static uc_token_t *
 parse_number(uc_lexer_t *lex)
 {
-	const struct token *tok = lex->tok;
 	uc_token_t *rv = NULL;
-	long long int n;
-	char *ptr, *e;
-	double d;
+	uc_value_t *nv = NULL;
+	const char *ptr;
+	char *e;
 
 	if (!buf_remaining(lex) || !is_numeric_char(lex, lex->bufstart[0])) {
-		if (lex->lookbehindlen == 0 && !is_numeric_char(lex, lex->bufstart[0]))
-			return emit_op(lex, lex->source->off, TK_SUB, NULL);
-
 		lookbehind_append(lex, "\0", 1);
 
-		n = strtoll(lex->lookbehind, &e, 0);
+		nv = uc_number_parse(lex->lookbehind, &e);
 
-		if (*e == '.' || *e == 'e' || *e == 'E') {
-			d = strtod(lex->lookbehind, &e);
+		switch (ucv_type(nv)) {
+		case UC_DOUBLE:
+			rv = emit_op(lex, lex->source->off - (e - lex->lookbehind), TK_DOUBLE, nv);
+			break;
 
-			if (tok->u.pat[0] == '-')
-				d = -d;
+		case UC_INTEGER:
+			rv = emit_op(lex, lex->source->off - (e - lex->lookbehind), TK_NUMBER, nv);
+			break;
 
-			if (e > lex->lookbehind && *e == 0)
-				rv = emit_op(lex, lex->source->off - (e - lex->lookbehind), TK_DOUBLE, ucv_double_new(d));
-			else
-				rv = emit_op(lex, lex->source->off - (lex->lookbehindlen - (e - lex->lookbehind) - 1), TK_ERROR, ucv_string_new("Invalid number literal"));
-		}
-		else if (*e == 0) {
-			if (tok->u.pat[0] == '-')
-				n = (errno == ERANGE) ? INT64_MIN : -n;
-
-			rv = emit_op(lex, lex->source->off - (e - lex->lookbehind), TK_NUMBER, ucv_int64_new(n));
-			//OP(rv)->is_overflow = (errno == ERANGE);
-		}
-		else {
+		default:
 			rv = emit_op(lex, lex->source->off - (lex->lookbehindlen - (e - lex->lookbehind) - 1), TK_ERROR, ucv_string_new("Invalid number literal"));
 		}
 

lib.c
@@ -216,52 +216,6 @@ static char *uc_cast_string(uc_vm_t *vm, uc_value_t **v, bool *freeable) {
 	return ucv_to_string(vm, *v);
 }
 
-static double
-uc_cast_double(uc_value_t *v)
-{
-	uc_type_t t;
-	int64_t n;
-	double d;
-
-	t = ucv_cast_number(v, &n, &d);
-	errno = 0;
-
-	if (t == UC_DOUBLE) {
-		if (isnan(d))
-			errno = EINVAL;
-		else if (!isfinite(d))
-			errno = EOVERFLOW;
-
-		return d;
-	}
-
-	return (double)n;
-}
-
-static int64_t
-uc_cast_int64(uc_value_t *v)
-{
-	uc_type_t t;
-	int64_t n;
-	double d;
-
-	t = ucv_cast_number(v, &n, &d);
-	errno = 0;
-
-	if (t == UC_DOUBLE) {
-		if (isnan(d))
-			errno = EINVAL;
-		else if (!isfinite(d))
-			errno = EOVERFLOW;
-		else if ((double)(int64_t)d != d)
-			errno = ERANGE;
-
-		return (int64_t)d;
-	}
-
-	return n;
-}
-
 static void
 uc_vm_ctx_push(uc_vm_t *vm)
 {
@@ -339,7 +293,7 @@ uc_index(uc_vm_t *vm, size_t nargs, bool right)
 	switch (ucv_type(stack)) {
 	case UC_ARRAY:
 		for (arridx = 0, len = ucv_array_length(stack); arridx < len; arridx++) {
-			if (ucv_compare(I_EQ, ucv_array_get(stack, arridx), needle)) {
+			if (ucv_compare(I_EQ, ucv_array_get(stack, arridx), needle, NULL)) {
 				ret = (ssize_t)arridx;
 
 				if (!right)
@@ -448,7 +402,7 @@ uc_chr(uc_vm_t *vm, size_t nargs)
 	str = xalloc(nargs);
 
 	for (idx = 0; idx < nargs; idx++) {
-		n = uc_cast_int64(uc_fn_arg(idx));
+		n = ucv_to_integer(uc_fn_arg(idx));
 
 		if (n < 0)
 			n = 0;
@@ -505,7 +459,7 @@ uc_exists(uc_vm_t *vm, size_t nargs)
 static uc_value_t *
 uc_exit(uc_vm_t *vm, size_t nargs)
 {
-	int64_t n = uc_cast_int64(uc_fn_arg(0));
+	int64_t n = ucv_to_integer(uc_fn_arg(0));
 
 	vm->arg.s32 = (int32_t)n;
 	uc_vm_raise_exception(vm, EXCEPTION_EXIT, "Terminated");
@@ -583,9 +537,9 @@ uc_hex(uc_vm_t *vm, size_t nargs)
 static uc_value_t *
 uc_int(uc_vm_t *vm, size_t nargs)
 {
-	int64_t n = uc_cast_int64(uc_fn_arg(0));
+	int64_t n = ucv_to_integer(uc_fn_arg(0));
 
-	if (errno == EINVAL || errno == EOVERFLOW)
+	if (errno == EINVAL || errno == ERANGE)
 		return ucv_double_new(NAN);
 
 	return ucv_int64_new(n);
@@ -796,59 +750,37 @@ static struct {
 static int
 default_cmp(uc_value_t *v1, uc_value_t *v2)
 {
-	uc_type_t t1, t2;
-	int64_t n1, n2;
-	double d1, d2;
 	char *s1, *s2;
 	bool f1, f2;
+	int res;
 
-	if (ucv_type(v1) == UC_INTEGER || ucv_type(v1) == UC_DOUBLE ||
-	    ucv_type(v2) == UC_INTEGER || ucv_type(v2) == UC_DOUBLE) {
-		t1 = ucv_cast_number(v1, &n1, &d1);
-		t2 = ucv_cast_number(v2, &n2, &d2);
+	/* when both operands are numeric then compare numerically */
+	if ((ucv_type(v1) == UC_INTEGER || ucv_type(v1) == UC_DOUBLE) &&
+	    (ucv_type(v2) == UC_INTEGER || ucv_type(v2) == UC_DOUBLE)) {
+		ucv_compare(0, v1, v2, &res);
 
-		if (t1 == UC_DOUBLE || t2 == UC_DOUBLE) {
-			d1 = (t1 == UC_DOUBLE) ? d1 : (double)n1;
-			d2 = (t2 == UC_DOUBLE) ? d2 : (double)n2;
-
-			if (d1 < d2)
-				return -1;
-
-			if (d1 > d2)
-				return 1;
-
-			return 0;
-		}
-
-		if (n1 < n2)
-			return -1;
-
-		if (n1 > n2)
-			return 1;
-
-		return 0;
+		return res;
 	}
 
+	/* otherwise convert both operands to strings and compare lexically */
 	s1 = uc_cast_string(sort_ctx.vm, &v1, &f1);
 	s2 = uc_cast_string(sort_ctx.vm, &v2, &f2);
 
-	n1 = strcmp(s1, s2);
+	res = strcmp(s1, s2);
 
 	if (f1) free(s1);
 	if (f2) free(s2);
 
-	return n1;
+	return res;
 }
 
 static int
 sort_fn(const void *k1, const void *k2)
 {
+	uc_value_t *rv, *null = ucv_int64_new(0);
 	uc_value_t * const *v1 = k1;
 	uc_value_t * const *v2 = k2;
-	uc_value_t *rv;
-	uc_type_t t;
-	int64_t n;
-	double d;
+	int res;
 
 	if (!sort_ctx.fn)
 		return default_cmp(*v1, *v2);
@@ -868,26 +800,13 @@ sort_fn(const void *k1, const void *k2)
 	}
 
 	rv = uc_vm_stack_pop(sort_ctx.vm);
-	t = ucv_cast_number(rv, &n, &d);
 
-	if (t == UC_DOUBLE) {
-		if (d < 0)
-			n = -1;
+	ucv_compare(0, rv, null, &res);
 
-		if (d > 0)
-			n = 1;
-	}
-	else {
-		if (n < 0)
-			n = -1;
-
-		if (n > 0)
-			n = 1;
-	}
-
+	ucv_put(null);
 	ucv_put(rv);
 
-	return n;
+	return res;
 }
 
 static uc_value_t *
@@ -911,8 +830,8 @@ static uc_value_t *
 uc_splice(uc_vm_t *vm, size_t nargs)
 {
 	uc_value_t *arr = uc_fn_arg(0);
-	int64_t ofs = uc_cast_int64(uc_fn_arg(1));
-	int64_t remlen = uc_cast_int64(uc_fn_arg(2));
+	int64_t ofs = ucv_to_integer(uc_fn_arg(1));
+	int64_t remlen = ucv_to_integer(uc_fn_arg(2));
 	size_t arrlen, addlen, idx;
 
 	if (ucv_type(arr) != UC_ARRAY)
@@ -1043,8 +962,8 @@ static uc_value_t *
 uc_substr(uc_vm_t *vm, size_t nargs)
 {
 	uc_value_t *str = uc_fn_arg(0);
-	int64_t ofs = uc_cast_int64(uc_fn_arg(1));
-	int64_t sublen = uc_cast_int64(uc_fn_arg(2));
+	int64_t ofs = ucv_to_integer(uc_fn_arg(1));
+	int64_t sublen = ucv_to_integer(uc_fn_arg(2));
 	const char *p;
 	size_t len;
 
@@ -1142,9 +1061,9 @@ uc_uchr(uc_vm_t *vm, size_t nargs)
 	int rem;
 
 	for (idx = 0, ulen = 0; idx < nargs; idx++) {
-		n = uc_cast_int64(uc_fn_arg(idx));
+		n = ucv_to_integer(uc_fn_arg(idx));
 
-		if (errno == EINVAL || errno == EOVERFLOW || n < 0 || n > 0x10FFFF)
+		if (errno == EINVAL || errno == ERANGE || n < 0 || n > 0x10FFFF)
 			ulen += 3;
 		else if (n <= 0x7F)
 			ulen++;
@@ -1159,9 +1078,9 @@ uc_uchr(uc_vm_t *vm, size_t nargs)
 	str = xalloc(ulen);
 
 	for (idx = 0, p = str, rem = ulen; idx < nargs; idx++) {
-		n = uc_cast_int64(uc_fn_arg(idx));
+		n = ucv_to_integer(uc_fn_arg(idx));
 
-		if (errno == EINVAL || errno == EOVERFLOW || n < 0 || n > 0x10FFFF)
+		if (errno == EINVAL || errno == ERANGE || n < 0 || n > 0x10FFFF)
 			n = 0xFFFD;
 
 		if (!utf8enc(&p, &rem, n))
@@ -1344,10 +1263,17 @@ uc_printf_common(uc_vm_t *vm, size_t nargs, uc_stringbuf_t *buf)
 			case 'X':
 				t = UC_INTEGER;
 
-				if (argidx < nargs)
-					arg.n = uc_cast_int64(uc_fn_arg(argidx++));
-				else
+				if (argidx < nargs) {
+					arg.n = ucv_to_integer(uc_fn_arg(argidx));
+
+					if (errno == ERANGE)
+						arg.n = (int64_t)ucv_to_unsigned(uc_fn_arg(argidx));
+
+					argidx++;
+				}
+				else {
 					arg.n = 0;
+				}
 
 				break;
 
@@ -1360,7 +1286,7 @@ uc_printf_common(uc_vm_t *vm, size_t nargs, uc_stringbuf_t *buf)
 				t = UC_DOUBLE;
 
 				if (argidx < nargs)
-					arg.d = uc_cast_double(uc_fn_arg(argidx++));
+					arg.d = ucv_to_double(uc_fn_arg(argidx++));
 				else
 					arg.d = 0;
 
@@ -1370,7 +1296,7 @@ uc_printf_common(uc_vm_t *vm, size_t nargs, uc_stringbuf_t *buf)
 				t = UC_INTEGER;
 
 				if (argidx < nargs)
-					arg.n = uc_cast_int64(uc_fn_arg(argidx++)) & 0xff;
+					arg.n = ucv_to_integer(uc_fn_arg(argidx++)) & 0xff;
 				else
 					arg.n = 0;
 
@@ -2436,7 +2362,7 @@ uc_sleep(uc_vm_t *vm, size_t nargs)
 	struct timeval tv;
 	int64_t ms;
 
-	ms = uc_cast_int64(duration);
+	ms = ucv_to_integer(duration);
 
 	if (errno != 0 || ms <= 0)
 		return ucv_boolean_new(false);
@@ -2562,7 +2488,7 @@ uc_sourcepath(uc_vm_t *vm, size_t nargs)
 	int64_t depth;
 	size_t i;
 
-	depth = uc_cast_int64(calldepth);
+	depth = ucv_to_integer(calldepth);
 
 	if (errno)
 		depth = 0;
@@ -2604,7 +2530,7 @@ uc_min_max(uc_vm_t *vm, size_t nargs, int cmp)
 	for (i = 0; i < nargs; i++) {
 		val = uc_fn_arg(i);
 
-		if (!set || ucv_compare(cmp, val, rv)) {
+		if (!set || ucv_compare(cmp, val, rv, NULL)) {
 			set = true;
 			rv = val;
 		}

lib/math.c
@@ -15,6 +15,7 @@
  */
 
 #include <math.h>
+#include <errno.h>
 #include <sys/time.h>
 
 #include "ucode/module.h"
@@ -24,20 +25,43 @@ static bool srand_called = false;
 static uc_value_t *
 uc_abs(uc_vm_t *vm, size_t nargs)
 {
-	uc_value_t *v = uc_fn_arg(0);
-	uc_type_t t;
+	uc_value_t *v = uc_fn_arg(0), *nv, *res;
 	int64_t n;
 	double d;
 
-	if (ucv_type(v) == UC_NULL)
-		return ucv_double_new(NAN);
+	nv = v ? ucv_to_number(v) : NULL;
 
-	t = ucv_cast_number(v, &n, &d);
+	switch (ucv_type(nv)) {
+	case UC_INTEGER:
+		n = ucv_int64_get(nv);
 
-	if (t == UC_DOUBLE)
-		return (isnan(d) || d < 0) ? ucv_double_new(-d) : ucv_get(v);
+		if (n >= 0 || errno == ERANGE)
+			res = ucv_get(nv);
+		else if (n == INT64_MIN)
+			res = ucv_uint64_new((uint64_t)INT64_MAX + 1);
+		else
+			res = ucv_uint64_new(-n);
 
-	return (n < 0) ? ucv_int64_new(-n) : ucv_get(v);
+		break;
+
+	case UC_DOUBLE:
+		d = ucv_double_get(nv);
+
+		if (isnan(d) || d >= 0)
+			res = ucv_get(nv);
+		else
+			res = ucv_double_new(-d);
+
+		break;
+
+	default:
+		res = ucv_double_new(NAN);
+		break;
+	}
+
+	ucv_put(nv);
+
+	return res;
 }
 
 static uc_value_t *

commit 3f6d1998b60e2778730a925d2b761db3b54979fe

vallist: uc_number_parse(): parse empty strings as `0`, not `NaN`
Fixes: b605dbf ("treewide: rework numeric value handling")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

vallist.c
@@ -62,7 +62,7 @@ uc_number_parse(const char *buf, char **end)
 		p++;
 	}
 
-	if (!isxdigit(*p))
+	if (*p != 0 && !isxdigit(*p))
 		return NULL;
 
 	if (!end)
@@ -73,7 +73,7 @@ uc_number_parse(const char *buf, char **end)
 	if (**end == '.' || **end == 'e' || **end == 'E') {
 		d = strtod(p, end);
 
-		if (*end == p || (!isspace(**end) && **end != 0))
+		if (!isspace(**end) && **end != 0)
 			return NULL;
 
 		if (neg)
@@ -82,7 +82,7 @@ uc_number_parse(const char *buf, char **end)
 		return ucv_double_new(d);
 	}
 
-	if (*end == p || (!isspace(**end) && **end != 0))
+	if (!isspace(**end) && **end != 0)
 		return NULL;
 
 	if (neg) {

tests/custom/04_bugs/29_empty_string_as_number
@@ -1,0 +1,17 @@
+When an empty string was casted to a number, e.g. explicitly through `+`
+or `int()` or implicitly through numerical calculations, it was incorrectly
+treated as `NaN` and not `0`.
+
+-- Testcase --
+{{ +"" }}
+{{ int("") }}
+{{ "" + 0 }}
+{{ "" - 0.0 }}
+-- End --
+
+-- Expect stdout --
+0
+0
+0
+0
+-- End --