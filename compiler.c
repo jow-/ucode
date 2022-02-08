jow-/ucode

commit b3d758bcacffa426c2bb2c2384961649b10ec7ca

compiler: fix for/break miscompilation
When patching jump targets for break statments while compiling for-loop
statments, jump beyond the instructions popping intermediate loop variables
off the stack to fix a stack position mismatch between compiler and vm.

Before that change, local loop body variables got popped twice, breaking
the expected stack layout.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

compiler.c
@@ -2128,9 +2128,6 @@ uc_compiler_compile_for_in(uc_compiler *compiler, bool local, uc_token *kvar, uc
 	/* back patch conditional jump */
 	uc_compiler_set_jmpaddr(compiler, test_jmp, chunk->count);
 
-	/* patch up break/continue */
-	uc_compiler_backpatch(compiler, chunk->count, skip_jmp + 5);
-
 	/* pop loop variables */
 	uc_compiler_emit_insn(compiler, 0, I_POP);
 
@@ -2138,6 +2135,9 @@ uc_compiler_compile_for_in(uc_compiler *compiler, bool local, uc_token *kvar, uc
 		uc_compiler_emit_insn(compiler, 0, I_POP);
 
 	uc_compiler_leave_scope(compiler);
+
+	/* patch up break/continue */
+	uc_compiler_backpatch(compiler, chunk->count, skip_jmp + 5);
 }
 
 static void
@@ -2232,10 +2232,10 @@ uc_compiler_compile_for_count(uc_compiler *compiler, bool local, uc_token *var)
 	if (test_off)
 		uc_compiler_set_jmpaddr(compiler, test_off, chunk->count);
 
+	uc_compiler_leave_scope(compiler);
+
 	/* patch up break/continue */
 	uc_compiler_backpatch(compiler, chunk->count, incr_off);
-
-	uc_compiler_leave_scope(compiler);
 }
 
 static void

tests/03_bugs/10_break_stack_mismatch
@@ -1,0 +1,38 @@
+When emitting jump instructions for breaking out of for-loops, the compiler
+incorrectly set the jump target before the pop instruction clearing the
+intermediate loop variables. Since the break instruction itself already
+compiles to a series of pop instructions reverting the stack to it's the
+pre-loop state, intermediate values got popped twice, leading to a stack
+layout mismatch between compiler and VM, resulting in wrong local variable
+values or segmentation faults at runtime.
+
+-- Testcase --
+{%
+	let x = 1;
+
+	for (let y in [2])
+		break;
+
+	print(x, "\n");
+%}
+-- End --
+
+-- Expect stdout --
+1
+-- End --
+
+
+-- Testcase --
+{%
+	let x = 1;
+
+	for (let y = 0; y < 1; y++)
+		break;
+
+	print(x, "\n");
+%}
+-- End --
+
+-- Expect stdout --
+1
+-- End --

commit a317c17f5ddfc3f749d349de01eeea5cad3eb162

compiler: fix incorrect loop break targets
When patching jump targets for break statments while compiling for-loop
statments, we need jump beyond the instructions popping intermediate loop
variables off the stack but before the pop instructions removing local
loop body variables to prevent a stack position mismatch between compiler
and vm.

Before that change, local loop body variables remained on the stack,
breaking the expected stack layout.

Fixes: b3d758b compiler: ("fix for/break miscompilation")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

compiler.c
@@ -2242,10 +2242,10 @@ uc_compiler_compile_for_in(uc_compiler_t *compiler, bool local, uc_token_t *kvar
 	if (vvar)
 		uc_compiler_emit_insn(compiler, 0, I_POP);
 
-	uc_compiler_leave_scope(compiler);
-
 	/* patch up break/continue */
 	uc_compiler_backpatch(compiler, chunk->count, skip_jmp + 5);
+
+	uc_compiler_leave_scope(compiler);
 }
 
 static void
@@ -2355,10 +2355,10 @@ uc_compiler_compile_for_count(uc_compiler_t *compiler, bool local, uc_token_t *v
 	if (test_off)
 		uc_compiler_set_jmpaddr(compiler, test_off, chunk->count);
 
-	uc_compiler_leave_scope(compiler);
-
 	/* patch up break/continue */
 	uc_compiler_backpatch(compiler, chunk->count, incr_off);
+
+	uc_compiler_leave_scope(compiler);
 }
 
 static void

tests/custom/04_bugs/10_break_stack_mismatch
@@ -13,12 +13,14 @@ values or segmentation faults at runtime.
 	for (let y in [2])
 		break;
 
-	print(x, "\n");
+	let z = 3;
+
+	print([ x, z ], "\n");
 %}
 -- End --
 
 -- Expect stdout --
-1
+[ 1, 3 ]
 -- End --
 
 
@@ -29,10 +31,12 @@ values or segmentation faults at runtime.
 	for (let y = 0; y < 1; y++)
 		break;
 
-	print(x, "\n");
+	let z = 3;
+
+	print([ x, z ], "\n");
 %}
 -- End --
 
 -- Expect stdout --
-1
+[ 1, 3 ]
 -- End --