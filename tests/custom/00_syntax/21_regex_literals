jow-/ucode

commit a45f2a388efb649e0373a45c6db1d009dc18072d

lexer: improve regex literal handling
 - Do not treat slashes within bracket expressions as delimitters
 - Do not escape slashes when stringifying regex sources
 - Allow all escape sequence types in regex literals

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

lexer.c
@@ -209,10 +209,118 @@ append_utf8(uc_lexer_t *lex, int code) {
 }
 
 static uc_token_t *
-parse_string(uc_lexer_t *lex, int kind)
+parse_escape(uc_lexer_t *lex, const char *retain)
 {
 	int code, ch, i;
+
+	/* unicode escape sequence */
+	if (check_char(lex, 'u')) {
+		for (i = 0, code = 0; i < 4; i++) {
+			ch = next_char(lex);
+
+			if (!isxdigit(ch))
+				return emit_op(lex, -1, TK_ERROR, ucv_string_new("Invalid escape sequence"));
+
+			code = code * 16 + hex(ch);
+		}
+
+		/* is a leading surrogate value */
+		if ((code & 0xFC00) == 0xD800) {
+			/* found a subsequent leading surrogate, ignore and emit replacement char for previous one */
+			if (lex->lead_surrogate)
+				append_utf8(lex, 0xFFFD);
+
+			/* store surrogate value and advance to next escape sequence */
+			lex->lead_surrogate = code;
+		}
+
+		/* is a trailing surrogate value */
+		else if ((code & 0xFC00) == 0xDC00) {
+			/* found a trailing surrogate following a leading one, combine and encode */
+			if (lex->lead_surrogate) {
+				code = 0x10000 + ((lex->lead_surrogate & 0x3FF) << 10) + (code & 0x3FF);
+				lex->lead_surrogate = 0;
+			}
+
+			/* trailing surrogate not following a leading one, ignore and use replacement char */
+			else {
+				code = 0xFFFD;
+			}
+
+			append_utf8(lex, code);
+		}
+
+		/* is a normal codepoint */
+		else {
+			append_utf8(lex, code);
+		}
+	}
+
+	/* hex escape sequence */
+	else if (check_char(lex, 'x')) {
+		for (i = 0, code = 0; i < 2; i++) {
+			ch = next_char(lex);
+
+			if (!isxdigit(ch))
+				return emit_op(lex, -1, TK_ERROR, ucv_string_new("Invalid escape sequence"));
+
+			code = code * 16 + hex(ch);
+		}
+
+		append_utf8(lex, code);
+	}
+
+	/* octal or letter */
+	else {
+		/* try to parse octal sequence... */
+		for (i = 0, code = 0, ch = lookahead_char(lex);
+		     i < 3 && ch >= '0' && ch <= '7';
+		     i++, next_char(lex), ch = lookahead_char(lex)) {
+			code = code * 8 + dec(ch);
+		}
+
+		if (i) {
+			if (code > 255)
+				return emit_op(lex, -3, TK_ERROR, ucv_string_new("Invalid escape sequence"));
+
+			append_utf8(lex, code);
+		}
+
+		/* ... no octal sequence, handle other escape */
+		else {
+			ch = next_char(lex);
+
+			switch (ch) {
+			case 'a': uc_vector_push(&lex->buffer, '\a'); break;
+			case 'b': uc_vector_push(&lex->buffer, '\b'); break;
+			case 'e': uc_vector_push(&lex->buffer, '\033'); break;
+			case 'f': uc_vector_push(&lex->buffer, '\f'); break;
+			case 'n': uc_vector_push(&lex->buffer, '\n'); break;
+			case 'r': uc_vector_push(&lex->buffer, '\r'); break;
+			case 't': uc_vector_push(&lex->buffer, '\t'); break;
+			case 'v': uc_vector_push(&lex->buffer, '\v'); break;
+
+			case EOF:
+				return emit_op(lex, -2, TK_ERROR, ucv_string_new("Unterminated string"));
+
+			default:
+				if (strchr(retain, ch))
+					uc_vector_push(&lex->buffer, '\\');
+
+				uc_vector_push(&lex->buffer, ch);
+			}
+		}
+	}
+
+	return NULL;
+}
+
+static uc_token_t *
+parse_string(uc_lexer_t *lex, int kind)
+{
+	uc_token_t *err;
 	unsigned type;
+	int code, ch;
 	size_t off;
 
 	if (kind == '`')
@@ -237,107 +345,74 @@ parse_string(uc_lexer_t *lex, int kind)
 			uc_vector_push(&lex->buffer, '$');
 			break;
 
+		/* regexp bracket expression */
+		case '[':
+			uc_vector_push(&lex->buffer, '[');
+
+			if (type == TK_REGEXP) {
+				/* skip leading negation (^) */
+				if (check_char(lex, '^'))
+					uc_vector_push(&lex->buffer, '^');
+
+				/* skip leading `]` - it is literal and not closing the bracket expr */
+				if (check_char(lex, ']'))
+					uc_vector_push(&lex->buffer, ']');
+
+				/* read until closing `]` */
+				for (ch = next_char(lex); ch != EOF; ch = next_char(lex)) {
+					if (ch == '\\') {
+						err = parse_escape(lex, "^");
+
+						if (err)
+							return err;
+
+						continue;
+					}
+
+					uc_vector_push(&lex->buffer, ch);
+
+					if (ch == ']')
+						break;
+
+					/* skip nested char classes / equivalence classes / collating chars */
+					if (ch == '[') {
+						code = lookahead_char(lex);
+
+						if (code == ':' || code == '.' || code == '=') {
+							uc_vector_push(&lex->buffer, code);
+							next_char(lex);
+
+							for (ch = next_char(lex); ch != EOF; ch = next_char(lex)) {
+								if (ch == '\\') {
+									err = parse_escape(lex, "");
+
+									if (err)
+										return err;
+
+									continue;
+								}
+
+								uc_vector_push(&lex->buffer, ch);
+
+								if (ch == code && check_char(lex, ']')) {
+									uc_vector_push(&lex->buffer, ']');
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			break;
+
 		/* escape sequence */
 		case '\\':
-			/* unicode escape sequence */
-			if (type != TK_REGEXP && check_char(lex, 'u')) {
-				for (i = 0, code = 0; i < 4; i++) {
-					ch = next_char(lex);
+			err = parse_escape(lex,
+				(type == TK_REGEXP) ? "^.[$()|*+?{\\" : "");
 
-					if (!isxdigit(ch))
-						return emit_op(lex, -1, TK_ERROR, ucv_string_new("Invalid escape sequence"));
-
-					code = code * 16 + hex(ch);
-				}
-
-				/* is a leading surrogate value */
-				if ((code & 0xFC00) == 0xD800) {
-					/* found a subsequent leading surrogate, ignore and emit replacement char for previous one */
-					if (lex->lead_surrogate)
-						append_utf8(lex, 0xFFFD);
-
-					/* store surrogate value and advance to next escape sequence */
-					lex->lead_surrogate = code;
-				}
-
-				/* is a trailing surrogate value */
-				else if ((code & 0xFC00) == 0xDC00) {
-					/* found a trailing surrogate following a leading one, combine and encode */
-					if (lex->lead_surrogate) {
-						code = 0x10000 + ((lex->lead_surrogate & 0x3FF) << 10) + (code & 0x3FF);
-						lex->lead_surrogate = 0;
-					}
-
-					/* trailing surrogate not following a leading one, ignore and use replacement char */
-					else {
-						code = 0xFFFD;
-					}
-
-					append_utf8(lex, code);
-				}
-
-				/* is a normal codepoint */
-				else {
-					append_utf8(lex, code);
-				}
-			}
-
-			/* hex escape sequence */
-			else if (type != TK_REGEXP && check_char(lex, 'x')) {
-				for (i = 0, code = 0; i < 2; i++) {
-					ch = next_char(lex);
-
-					if (!isxdigit(ch))
-						return emit_op(lex, -1, TK_ERROR, ucv_string_new("Invalid escape sequence"));
-
-					code = code * 16 + hex(ch);
-				}
-
-				append_utf8(lex, code);
-			}
-
-			/* octal or letter */
-			else {
-				/* try to parse octal sequence... */
-				for (i = 0, code = 0, ch = lookahead_char(lex);
-				     kind != '/' && i < 3 && ch >= '0' && ch <= '7';
-				     i++, next_char(lex), ch = lookahead_char(lex)) {
-					code = code * 8 + dec(ch);
-				}
-
-				if (i) {
-					if (code > 255)
-						return emit_op(lex, -3, TK_ERROR, ucv_string_new("Invalid escape sequence"));
-
-					append_utf8(lex, code);
-				}
-
-				/* ... no octal sequence, handle other escape */
-				else {
-					ch = next_char(lex);
-
-					switch (ch) {
-					case 'a': uc_vector_push(&lex->buffer, '\a'); break;
-					case 'b': uc_vector_push(&lex->buffer, '\b'); break;
-					case 'e': uc_vector_push(&lex->buffer, '\033'); break;
-					case 'f': uc_vector_push(&lex->buffer, '\f'); break;
-					case 'n': uc_vector_push(&lex->buffer, '\n'); break;
-					case 'r': uc_vector_push(&lex->buffer, '\r'); break;
-					case 't': uc_vector_push(&lex->buffer, '\t'); break;
-					case 'v': uc_vector_push(&lex->buffer, '\v'); break;
-
-					case EOF:
-						return emit_op(lex, -2, TK_ERROR, ucv_string_new("Unterminated string"));
-
-					default:
-						/* regex mode => retain backslash */
-						if (type == TK_REGEXP)
-							uc_vector_push(&lex->buffer, '\\');
-
-						uc_vector_push(&lex->buffer, ch);
-					}
-				}
-			}
+			if (err)
+				return err;
 
 			break;
 

tests/custom/00_syntax/21_regex_literals
@@ -4,7 +4,7 @@ within regular expression literals is subject of the underlying
 regular expression engine.
 
 -- Expect stdout --
-[ "/Hello world/", "/test/gis", "/test/g", "/test1 \\\/ test2/", "/\\x31\n\\.\u0007\b\\c\\u2600\\\\/" ]
+[ "/Hello world/", "/test/gis", "/test/g", "/test1 / test2/", "/1\n\\.\u0007\bc☀\\\\/" ]
 -- End --
 
 -- Testcase --
@@ -91,5 +91,29 @@ In line 7, byte 30:
 		if (e.type == "Syntax error")
 			die("Catched syntax error");
 	}
+%}
+-- End --
+
+
+Testing that slashes within character classes are not treated as regex
+literal delimitters.
+
+-- Expect stdout --
+[
+	"/[/]/",
+	"/[[./.]/]/",
+	"/[[:alpha:]/]/",
+	"/[[=/=]/]/"
+]
+-- End --
+
+-- Testcase --
+{%
+	printf("%.J\n", [
+		/[/]/,
+		/[[./.]/]/,
+		/[[:alpha:]/]/,
+		/[[=/=]/]/
+	]);
 %}
 -- End --

types.c
@@ -1431,9 +1431,6 @@ ucv_to_string_json_encoded(uc_stringbuf_t *pb, const char *s, size_t len, bool r
 			break;
 
 		case '/':
-			if (regexp)
-				ucv_stringbuf_append(pb, "\\");
-
 			ucv_stringbuf_append(pb, "/");
 			break;
 

commit 21ace5e5c7c98271d78ff9cdf2b61e3ac70704d8

lexer: fixes for regex literal parsing
 - Ensure that regexp extension escapes are consistently handled;
   substitute `\d`, `\D`, `\s`, `\S`, `\w` and `\W` with `[[:digit:]]`,
   `[^[:digit:]]`, `[[:space:]]`, `[^[:space:]]`, `[[:alnum:]_]` and
   `[^[:alnum:]_]` character classes respectively since not all POSIX
   regexp implementations implement all of those extensions
 - Preserve `\b`, `\B`, `\<` and `\>` boundary matches

Fixes: a45f2a3 ("lexer: improve regex literal handling")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

lexer.c
@@ -209,9 +209,10 @@ append_utf8(uc_lexer_t *lex, int code) {
 }
 
 static uc_token_t *
-parse_escape(uc_lexer_t *lex, const char *retain)
+parse_escape(uc_lexer_t *lex, const char *regex_macros)
 {
 	int code, ch, i;
+	const char *p;
 
 	/* unicode escape sequence */
 	if (check_char(lex, 'u')) {
@@ -286,7 +287,31 @@ parse_escape(uc_lexer_t *lex, const char *retain)
 			append_utf8(lex, code);
 		}
 
-		/* ... no octal sequence, handle other escape */
+		/* ... no octal sequence, handle potential regex macros */
+		else if (strchr(regex_macros, ch)) {
+			ch = next_char(lex);
+
+			switch (ch) {
+			case 'd': p = "[[:digit:]]";   break;
+			case 'D': p = "[^[:digit:]]";  break;
+			case 'w': p = "[[:alnum:]_]";  break;
+			case 'W': p = "[^[:alnum:]_]"; break;
+			case 's': p = "[[:space:]]";   break;
+			case 'S': p = "[^[:space:]]";  break;
+			default:  p = NULL;
+			}
+
+			if (p) {
+				while (*p)
+					uc_vector_push(&lex->buffer, *p++);
+			}
+			else {
+				uc_vector_push(&lex->buffer, '\\');
+				uc_vector_push(&lex->buffer, ch);
+			}
+		}
+
+		/* ... handle other escape */
 		else {
 			ch = next_char(lex);
 
@@ -304,9 +329,6 @@ parse_escape(uc_lexer_t *lex, const char *retain)
 				return emit_op(lex, -2, TK_ERROR, ucv_string_new("Unterminated string"));
 
 			default:
-				if (strchr(retain, ch))
-					uc_vector_push(&lex->buffer, '\\');
-
 				uc_vector_push(&lex->buffer, ch);
 			}
 		}
@@ -409,7 +431,7 @@ parse_string(uc_lexer_t *lex, int kind)
 		/* escape sequence */
 		case '\\':
 			err = parse_escape(lex,
-				(type == TK_REGEXP) ? "^.[$()|*+?{\\" : "");
+				(type == TK_REGEXP) ? "^bBdDsSwW<>.[$()|*+?{\\" : "");
 
 			if (err)
 				return err;

tests/custom/00_syntax/21_regex_literals
@@ -4,7 +4,7 @@ within regular expression literals is subject of the underlying
 regular expression engine.
 
 -- Expect stdout --
-[ "/Hello world/", "/test/gis", "/test/g", "/test1 / test2/", "/1\n\\.\u0007\bc☀\\\\/" ]
+[ "/Hello world/", "/test/gis", "/test/g", "/test1 / test2/", "/1\n\\.\u0007\\bc☀\\\\/" ]
 -- End --
 
 -- Testcase --
@@ -114,6 +114,33 @@ literal delimitters.
 		/[[./.]/]/,
 		/[[:alpha:]/]/,
 		/[[=/=]/]/
+	]);
+%}
+-- End --
+
+
+Testing that regex extension macros are substituted only outside of
+bracket set expressions.
+
+-- Expect stdout --
+[
+	"/ \\b \\B [\b B] /",
+	"/ \\< \\> [< >] /",
+	"/ [[:digit:]] [^[:digit:]] [d D] /",
+	"/ [[:space:]] [^[:space:]] [s S] /",
+	"/ [[:alnum:]_] [^[:alnum:]_] [w W] /"
+]
+-- End --
+
+-- Testcase --
+{%
+	printf("%.J\n", [
+		/ \b \B [\b \B] /,   // \b outside brackets is a word boundary,
+		                     // \b within brackets is backspace
+		/ \< \> [\< \>] /,
+		/ \d \D [\d \D] /,
+		/ \s \S [\s \S] /,
+		/ \w \W [\w \W] /
 	]);
 %}
 -- End --