jow-/ucode

commit fdc9b6a3841d7346dab6f4aeea06322be0d3ee95

compiler: fix `??=`, `||=` and `&&=` logical assignment semantics
When compiling logical assignment expressions, ensure that the right hand
side of the assignment is not evaluated when the assignment condition is
unfulfilled.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

compiler.c
@@ -1092,10 +1092,7 @@ uc_compiler_emit_variable_rw(uc_compiler_t *compiler, uc_value_t *varname, uc_to
 	case TK_ASBOR:     sub_insn = I_BOR;     break;
 	case TK_ASLEFT:    sub_insn = I_LSHIFT;  break;
 	case TK_ASRIGHT:   sub_insn = I_RSHIFT;  break;
-	case TK_ASAND:     sub_insn = I_LTRUE;   break;
-	case TK_ASOR:      sub_insn = I_LFALSE;  break;
 	case TK_ASEXP:     sub_insn = I_EXP;     break;
-	case TK_ASNULLISH: sub_insn = I_LNULL;   break;
 	default:           sub_insn = 0;         break;
 	}
 
@@ -1153,6 +1150,100 @@ uc_compiler_emit_variable_rw(uc_compiler_t *compiler, uc_value_t *varname, uc_to
 }
 
 static void
+uc_compiler_emit_variable_copy(uc_compiler_t *compiler, uc_value_t *var)
+{
+	if (!var) {
+		uc_compiler_emit_insn(compiler, 0, I_COPY);
+		uc_compiler_emit_u8(compiler, 0, 1);
+		uc_compiler_emit_insn(compiler, 0, I_COPY);
+		uc_compiler_emit_u8(compiler, 0, 1);
+	}
+
+	uc_compiler_emit_variable_rw(compiler, var, 0);
+}
+
+static void
+uc_compiler_compile_and_common(uc_compiler_t *compiler, bool assignment, uc_value_t *var)
+{
+	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
+	size_t jmpz_off;
+
+	if (assignment)
+		uc_compiler_emit_variable_copy(compiler, var);
+
+	uc_compiler_emit_insn(compiler, 0, I_COPY);
+	uc_compiler_emit_u8(compiler, 0, 0);
+	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
+	uc_compiler_emit_insn(compiler, 0, I_POP);
+
+	if (assignment) {
+		uc_compiler_parse_precedence(compiler, P_ASSIGN);
+		uc_compiler_emit_variable_rw(compiler, var, TK_ASSIGN);
+	}
+	else {
+		uc_compiler_parse_precedence(compiler, P_AND);
+	}
+
+	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
+}
+
+static void
+uc_compiler_compile_or_common(uc_compiler_t *compiler, bool assignment, uc_value_t *var)
+{
+	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
+	size_t jmpz_off, jmp_off;
+
+	if (assignment)
+		uc_compiler_emit_variable_copy(compiler, var);
+
+	uc_compiler_emit_insn(compiler, 0, I_COPY);
+	uc_compiler_emit_u8(compiler, 0, 0);
+	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
+	jmp_off = uc_compiler_emit_jmp(compiler, 0);
+	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
+	uc_compiler_emit_insn(compiler, 0, I_POP);
+
+	if (assignment) {
+		uc_compiler_parse_precedence(compiler, P_ASSIGN);
+		uc_compiler_emit_variable_rw(compiler, var, TK_ASSIGN);
+	}
+	else {
+		uc_compiler_parse_precedence(compiler, P_OR);
+	}
+
+	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
+}
+
+static void
+uc_compiler_compile_nullish_common(uc_compiler_t *compiler, bool assignment, uc_value_t *var)
+{
+	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
+	size_t jmpz_off, jmp_off;
+
+	if (assignment)
+		uc_compiler_emit_variable_copy(compiler, var);
+
+	uc_compiler_emit_insn(compiler, 0, I_COPY);
+	uc_compiler_emit_u8(compiler, 0, 0);
+	uc_compiler_emit_insn(compiler, 0, I_LNULL);
+	uc_compiler_emit_insn(compiler, 0, I_NES);
+	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
+	jmp_off = uc_compiler_emit_jmp(compiler, 0);
+	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
+	uc_compiler_emit_insn(compiler, 0, I_POP);
+
+	if (assignment) {
+		uc_compiler_parse_precedence(compiler, P_ASSIGN);
+		uc_compiler_emit_variable_rw(compiler, var, TK_ASSIGN);
+	}
+	else {
+		uc_compiler_parse_precedence(compiler, P_OR);
+	}
+
+	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
+}
+
+static void
 uc_compiler_compile_expression(uc_compiler_t *compiler)
 {
 	uc_compiler_parse_precedence(compiler, P_COMMA);
@@ -1163,7 +1254,25 @@ uc_compiler_compile_assignment(uc_compiler_t *compiler, uc_value_t *var)
 {
 	uc_tokentype_t type = compiler->parser->curr.type;
 
-	if (uc_compiler_parse_at_assignment_op(compiler)) {
+	if (type == TK_ASNULLISH) {
+		uc_compiler_parse_advance(compiler);
+		uc_compiler_compile_nullish_common(compiler, true, var);
+
+		return true;
+	}
+	else if (type == TK_ASOR) {
+		uc_compiler_parse_advance(compiler);
+		uc_compiler_compile_or_common(compiler, true, var);
+
+		return true;
+	}
+	else if (type == TK_ASAND) {
+		uc_compiler_parse_advance(compiler);
+		uc_compiler_compile_and_common(compiler, true, var);
+
+		return true;
+	}
+	else if (uc_compiler_parse_at_assignment_op(compiler)) {
 		uc_compiler_parse_advance(compiler);
 		uc_compiler_parse_precedence(compiler, P_ASSIGN);
 		uc_compiler_emit_variable_rw(compiler, var, type);
@@ -1750,49 +1859,19 @@ uc_compiler_compile_funcdecl(uc_compiler_t *compiler)
 static void
 uc_compiler_compile_and(uc_compiler_t *compiler)
 {
-	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
-	size_t jmpz_off;
-
-	uc_compiler_emit_insn(compiler, 0, I_COPY);
-	uc_compiler_emit_u8(compiler, 0, 0);
-	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
-	uc_compiler_emit_insn(compiler, 0, I_POP);
-	uc_compiler_parse_precedence(compiler, P_AND);
-	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
+	return uc_compiler_compile_and_common(compiler, false, NULL);
 }
 
 static void
 uc_compiler_compile_or(uc_compiler_t *compiler)
 {
-	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
-	size_t jmpz_off, jmp_off;
-
-	uc_compiler_emit_insn(compiler, 0, I_COPY);
-	uc_compiler_emit_u8(compiler, 0, 0);
-	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
-	jmp_off = uc_compiler_emit_jmp(compiler, 0);
-	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
-	uc_compiler_emit_insn(compiler, 0, I_POP);
-	uc_compiler_parse_precedence(compiler, P_OR);
-	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
+	return uc_compiler_compile_or_common(compiler, false, NULL);
 }
 
 static void
 uc_compiler_compile_nullish(uc_compiler_t *compiler)
 {
-	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
-	size_t jmpz_off, jmp_off;
-
-	uc_compiler_emit_insn(compiler, 0, I_COPY);
-	uc_compiler_emit_u8(compiler, 0, 0);
-	uc_compiler_emit_insn(compiler, 0, I_LNULL);
-	uc_compiler_emit_insn(compiler, 0, I_NES);
-	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
-	jmp_off = uc_compiler_emit_jmp(compiler, 0);
-	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
-	uc_compiler_emit_insn(compiler, 0, I_POP);
-	uc_compiler_parse_precedence(compiler, P_OR);
-	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
+	return uc_compiler_compile_nullish_common(compiler, false, NULL);
 }
 
 static void

tests/custom/00_syntax/25_and_or_assignment
@@ -53,3 +53,33 @@ expression result if the lhs is falsy.
 	printf("%.J\n", [ x, y, z ]);
 %}
 -- End --
+
+
+3. Ensure that the assignment value expression is not evaluated if the
+assignment condition is false.
+
+-- Expect stdout --
+[
+	0,
+	0,
+	0
+]
+-- End --
+
+-- Testcase --
+{%
+	a = 0;
+	b = 0;
+	c = 0;
+
+	x = false;
+	y = false;
+	z = true;
+
+	x ??= a++;
+	y &&= b++;
+	z ||= c++;
+
+	printf("%.J\n", [ a, b, c ]);
+%}
+-- End --

vm.c
@@ -1409,31 +1409,6 @@ uc_vm_value_bitop(uc_vm_t *vm, uc_vm_insn_t operation, uc_value_t *value, uc_val
 }
 
 static uc_value_t *
-uc_vm_value_logical(uc_vm_t *vm, uc_vm_insn_t operation, uc_value_t *value, uc_value_t *operand)
-{
-	uc_value_t *rv = NULL;
-
-	switch (operation) {
-	case I_LTRUE:
-		rv = ucv_get(ucv_is_truish(value) ? operand : value);
-		break;
-
-	case I_LFALSE:
-		rv = ucv_get(ucv_is_truish(value) ? value : operand);
-		break;
-
-	case I_LNULL:
-		rv = ucv_get(value == NULL ? operand : value);
-		break;
-
-	default:
-		break;
-	}
-
-	return rv;
-}
-
-static uc_value_t *
 uc_vm_string_concat(uc_vm_t *vm, uc_value_t *v1, uc_value_t *v2)
 {
 	char buf[sizeof(void *)], *s1, *s2;
@@ -1491,9 +1466,6 @@ uc_vm_value_arith(uc_vm_t *vm, uc_vm_insn_t operation, uc_value_t *value, uc_val
 	if (operation == I_LSHIFT || operation == I_RSHIFT ||
 	    operation == I_BAND || operation == I_BXOR || operation == I_BOR)
 		return uc_vm_value_bitop(vm, operation, value, operand);
-
-	if (operation == I_LTRUE || operation == I_LFALSE || operation == I_LNULL)
-		return uc_vm_value_logical(vm, operation, value, operand);
 
 	if (operation == I_ADD && (ucv_type(value) == UC_STRING || ucv_type(operand) == UC_STRING))
 		return uc_vm_string_concat(vm, value, operand);

commit eac2add08377eb4ba8e43e7e6a8f385480ebd73b

compiler: fix bytecode for logical assignments of properties
The compiler emitted incorrect bytecode for logical assignment operations
on property expressions. The generated instructions left the stack in an
unclean state when the assignment condition was not fulfilled, causing a
stack layout mismatch between compiler and vm, leading to undefined
variable accesses and other non-deterministic behavior.

Solve this issue by rewriting the bytecode generation to yield an
instruction sequence that does not leave garbage on the stack.

The implementation is not optimal yet, as an expression in the form
`obj.prop ||= val` will load `obj.prop` twice. This is acceptable for
now as the load operation has no side effect, but should be solved in
a better way by introducing new instructions that allow for swapping
stack slots, allowing the vm to operate on a copy of the loaded value.

Also rewrite the corresponding test case to trigger a runtime error
on code versions before this fix.

Fixes: fdc9b6a ("compiler: fix `??=`, `||=` and `&&=` logical assignment semantics")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

compiler.c
@@ -575,6 +575,17 @@ uc_compiler_emit_jmp_dest(uc_compiler_t *compiler, size_t srcpos, uint32_t dest)
 	return chunk->count - 5;
 }
 
+static size_t
+uc_compiler_emit_copy(uc_compiler_t *compiler, size_t srcpos, uint8_t depth)
+{
+	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
+
+	uc_compiler_emit_insn(compiler, srcpos, I_COPY);
+	uc_compiler_emit_u8(compiler, 0, depth);
+
+	return chunk->count - 2;
+}
+
 static ssize_t
 uc_compiler_get_jmpaddr(uc_compiler_t *compiler, size_t off)
 {
@@ -1153,93 +1164,104 @@ static void
 uc_compiler_emit_variable_copy(uc_compiler_t *compiler, uc_value_t *var)
 {
 	if (!var) {
-		uc_compiler_emit_insn(compiler, 0, I_COPY);
-		uc_compiler_emit_u8(compiler, 0, 1);
-		uc_compiler_emit_insn(compiler, 0, I_COPY);
-		uc_compiler_emit_u8(compiler, 0, 1);
+		uc_compiler_emit_copy(compiler, 0, 1);
+		uc_compiler_emit_copy(compiler, 0, 1);
 	}
 
 	uc_compiler_emit_variable_rw(compiler, var, 0);
 }
 
 static void
-uc_compiler_compile_and_common(uc_compiler_t *compiler, bool assignment, uc_value_t *var)
+uc_compiler_compile_and(uc_compiler_t *compiler)
 {
 	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
 	size_t jmpz_off;
 
-	if (assignment)
-		uc_compiler_emit_variable_copy(compiler, var);
-
-	uc_compiler_emit_insn(compiler, 0, I_COPY);
-	uc_compiler_emit_u8(compiler, 0, 0);
+	uc_compiler_emit_copy(compiler, 0, 0);
 	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
 	uc_compiler_emit_insn(compiler, 0, I_POP);
-
-	if (assignment) {
-		uc_compiler_parse_precedence(compiler, P_ASSIGN);
-		uc_compiler_emit_variable_rw(compiler, var, TK_ASSIGN);
-	}
-	else {
-		uc_compiler_parse_precedence(compiler, P_AND);
-	}
-
+	uc_compiler_parse_precedence(compiler, P_AND);
 	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
 }
 
 static void
-uc_compiler_compile_or_common(uc_compiler_t *compiler, bool assignment, uc_value_t *var)
+uc_compiler_compile_and_assignment(uc_compiler_t *compiler, uc_value_t *var)
+{
+	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
+	size_t jmpz_off;
+
+	uc_compiler_emit_variable_copy(compiler, var);
+	uc_compiler_emit_copy(compiler, 0, 0);
+	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
+	uc_compiler_emit_insn(compiler, 0, I_POP);
+	uc_compiler_parse_precedence(compiler, P_ASSIGN);
+	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
+	uc_compiler_emit_variable_rw(compiler, var, TK_ASSIGN);
+}
+
+static void
+uc_compiler_compile_or(uc_compiler_t *compiler)
 {
 	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
 	size_t jmpz_off, jmp_off;
 
-	if (assignment)
-		uc_compiler_emit_variable_copy(compiler, var);
-
-	uc_compiler_emit_insn(compiler, 0, I_COPY);
-	uc_compiler_emit_u8(compiler, 0, 0);
+	uc_compiler_emit_copy(compiler, 0, 0);
 	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
 	jmp_off = uc_compiler_emit_jmp(compiler, 0);
 	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
 	uc_compiler_emit_insn(compiler, 0, I_POP);
-
-	if (assignment) {
-		uc_compiler_parse_precedence(compiler, P_ASSIGN);
-		uc_compiler_emit_variable_rw(compiler, var, TK_ASSIGN);
-	}
-	else {
-		uc_compiler_parse_precedence(compiler, P_OR);
-	}
-
+	uc_compiler_parse_precedence(compiler, P_OR);
 	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
 }
 
 static void
-uc_compiler_compile_nullish_common(uc_compiler_t *compiler, bool assignment, uc_value_t *var)
+uc_compiler_compile_or_assignment(uc_compiler_t *compiler, uc_value_t *var)
 {
 	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
 	size_t jmpz_off, jmp_off;
 
-	if (assignment)
-		uc_compiler_emit_variable_copy(compiler, var);
+	uc_compiler_emit_variable_copy(compiler, var);
+	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
+	uc_compiler_emit_variable_rw(compiler, var, 0);
+	jmp_off = uc_compiler_emit_jmp(compiler, 0);
+	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
+	uc_compiler_parse_precedence(compiler, P_ASSIGN);
+	uc_compiler_emit_variable_rw(compiler, var, TK_ASSIGN);
+	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
+}
 
-	uc_compiler_emit_insn(compiler, 0, I_COPY);
-	uc_compiler_emit_u8(compiler, 0, 0);
+static void
+uc_compiler_compile_nullish(uc_compiler_t *compiler)
+{
+	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
+	size_t jmpz_off, jmp_off;
+
+	uc_compiler_emit_copy(compiler, 0, 0);
 	uc_compiler_emit_insn(compiler, 0, I_LNULL);
 	uc_compiler_emit_insn(compiler, 0, I_NES);
 	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
 	jmp_off = uc_compiler_emit_jmp(compiler, 0);
 	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
 	uc_compiler_emit_insn(compiler, 0, I_POP);
+	uc_compiler_parse_precedence(compiler, P_OR);
+	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
+}
 
-	if (assignment) {
-		uc_compiler_parse_precedence(compiler, P_ASSIGN);
-		uc_compiler_emit_variable_rw(compiler, var, TK_ASSIGN);
-	}
-	else {
-		uc_compiler_parse_precedence(compiler, P_OR);
-	}
+static void
+uc_compiler_compile_nullish_assignment(uc_compiler_t *compiler, uc_value_t *var)
+{
+	uc_chunk_t *chunk = uc_compiler_current_chunk(compiler);
+	size_t jmpz_off, jmp_off;
 
+	uc_compiler_emit_variable_copy(compiler, var);
+	uc_compiler_emit_insn(compiler, 0, I_LNULL);
+	uc_compiler_emit_insn(compiler, 0, I_NES);
+	jmpz_off = uc_compiler_emit_jmpz(compiler, 0);
+	uc_compiler_emit_variable_rw(compiler, var, 0);
+	jmp_off = uc_compiler_emit_jmp(compiler, 0);
+	uc_compiler_set_jmpaddr(compiler, jmpz_off, chunk->count);
+	uc_compiler_parse_precedence(compiler, P_ASSIGN);
+	uc_compiler_emit_variable_rw(compiler, var, TK_ASSIGN);
 	uc_compiler_set_jmpaddr(compiler, jmp_off, chunk->count);
 }
 
@@ -1256,19 +1278,19 @@ uc_compiler_compile_assignment(uc_compiler_t *compiler, uc_value_t *var)
 
 	if (type == TK_ASNULLISH) {
 		uc_compiler_parse_advance(compiler);
-		uc_compiler_compile_nullish_common(compiler, true, var);
+		uc_compiler_compile_nullish_assignment(compiler, var);
 
 		return true;
 	}
 	else if (type == TK_ASOR) {
 		uc_compiler_parse_advance(compiler);
-		uc_compiler_compile_or_common(compiler, true, var);
+		uc_compiler_compile_or_assignment(compiler, var);
 
 		return true;
 	}
 	else if (type == TK_ASAND) {
 		uc_compiler_parse_advance(compiler);
-		uc_compiler_compile_and_common(compiler, true, var);
+		uc_compiler_compile_and_assignment(compiler, var);
 
 		return true;
 	}
@@ -1850,24 +1872,6 @@ static void
 uc_compiler_compile_funcdecl(uc_compiler_t *compiler)
 {
 	return uc_compiler_compile_funcexpr_common(compiler, true);
-}
-
-static void
-uc_compiler_compile_and(uc_compiler_t *compiler)
-{
-	return uc_compiler_compile_and_common(compiler, false, NULL);
-}
-
-static void
-uc_compiler_compile_or(uc_compiler_t *compiler)
-{
-	return uc_compiler_compile_or_common(compiler, false, NULL);
-}
-
-static void
-uc_compiler_compile_nullish(uc_compiler_t *compiler)
-{
-	return uc_compiler_compile_nullish_common(compiler, false, NULL);
 }
 
 static void

tests/custom/00_syntax/25_and_or_assignment
@@ -10,6 +10,9 @@ expression result if the lhs is truish.
 [
 	null,
 	false,
+	"is truish",
+	null,
+	false,
 	"is truish"
 ]
 -- End --
@@ -20,11 +23,23 @@ expression result if the lhs is truish.
 	y = false;
 	z = true;
 
-	x &&= "is truish";
-	y &&= "is truish";
-	z &&= "is truish";
+	o = {
+		a: null,
+		b: false,
+		c: true
+	};
 
-	printf("%.J\n", [ x, y, z ]);
+	res = [];
+
+	push(res, x &&= "is truish");
+	push(res, y &&= "is truish");
+	push(res, z &&= "is truish");
+
+	push(res, o.a &&= "is truish");
+	push(res, o.b &&= "is truish");
+	push(res, o.c &&= "is truish");
+
+	printf("%.J\n", res);
 %}
 -- End --
 
@@ -34,6 +49,9 @@ expression result if the lhs is falsy.
 
 -- Expect stdout --
 [
+	"is falsy",
+	"is falsy",
+	true,
 	"is falsy",
 	"is falsy",
 	true
@@ -46,11 +64,23 @@ expression result if the lhs is falsy.
 	y = false;
 	z = true;
 
-	x ||= "is falsy";
-	y ||= "is falsy";
-	z ||= "is falsy";
+	o = {
+		a: null,
+		b: false,
+		c: true
+	};
 
-	printf("%.J\n", [ x, y, z ]);
+	res = [];
+
+	push(res, x ||= "is falsy");
+	push(res, y ||= "is falsy");
+	push(res, z ||= "is falsy");
+
+	push(res, o.a ||= "is falsy");
+	push(res, o.b ||= "is falsy");
+	push(res, o.c ||= "is falsy");
+
+	printf("%.J\n", res);
 %}
 -- End --
 
@@ -60,6 +90,15 @@ assignment condition is false.
 
 -- Expect stdout --
 [
+	false,
+	false,
+	true,
+	false,
+	false,
+	true,
+	0,
+	0,
+	0,
 	0,
 	0,
 	0
@@ -71,15 +110,30 @@ assignment condition is false.
 	a = 0;
 	b = 0;
 	c = 0;
+	d = 0;
+	e = 0;
+	f = 0;
+
+	o = {
+		a: false,
+		b: false,
+		c: true
+	};
 
 	x = false;
 	y = false;
 	z = true;
 
-	x ??= a++;
-	y &&= b++;
-	z ||= c++;
+	res = [];
 
-	printf("%.J\n", [ a, b, c ]);
+	push(res, x ??= a++);
+	push(res, y &&= b++);
+	push(res, z ||= c++);
+
+	push(res, o.a ??= d++);
+	push(res, o.b &&= e++);
+	push(res, o.c ||= f++);
+
+	printf("%.J\n", [ ...res, a, b, c, d, e, f ]);
 %}
 -- End --