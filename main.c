jow-/ucode

commit 0a7ff4715cb8ec863ed061443aa77f986c840507

main: pretty-print `-p` output by default
When outputting non-string expression results while running the cli
interpreter using the `-p` flag, pretty-print the resulting JSON data
for better readability.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

main.c
@@ -145,11 +145,18 @@ compile(uc_vm_t *vm, uc_source_t *src, FILE *precompile, bool strip, char *inter
 	switch (rc) {
 	case STATUS_OK:
 		if (print_result) {
-			uc_vm_stack_push(vm, res);
-			uc_vm_stack_push(vm, ucv_string_new("\n"));
-			uc_stdlib_function("print")(vm, 2);
-			uc_vm_stack_pop(vm);
-			uc_vm_stack_pop(vm);
+			if (ucv_type(res) == UC_STRING) {
+				fwrite(ucv_string_get(res), ucv_string_length(res), 1, stdout);
+			}
+			else {
+				uc_stringbuf_t *pb = xprintbuf_new();
+
+				ucv_to_stringbuf_formatted(vm, pb, res, 0, '\t', 1);
+				fwrite(pb->buf, pb->bpos, 1, stdout);
+				printbuf_free(pb);
+			}
+
+			ucv_put(res);
 		}
 
 		rc = 0;

commit 4efad5388184a2fd76957f0300eeab1a3b1eee2f

main: fix crash on printing -p output
Delete a duplicate ucv_put() line that can lead to a double-free bug.

Reproduced by running:
ucode -l nl80211 -p 'sprintf("%.J\n",
    nl80211.request(nl80211.const.NL80211_CMD_GET_WIPHY,
                    nl80211.const.NLM_F_DUMP,
		    { wiphy: 0, split_wiphy_dump: true }))'

Fixes: 0a7ff4715cb8 ("main: pretty-print `-p` output by default")
Signed-off-by: Felix Fietkau <nbd@nbd.name>

main.c
@@ -155,8 +155,6 @@ compile(uc_vm_t *vm, uc_source_t *src, FILE *precompile, bool strip, char *inter
 				fwrite(pb->buf, pb->bpos, 1, stdout);
 				printbuf_free(pb);
 			}
-
-			ucv_put(res);
 		}
 
 		rc = 0;