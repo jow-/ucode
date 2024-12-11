jow-/ucode

commit f9d2faf67de643911f7b9bcdbab1ab2d7d108dff

vm: reset signals when freeing VM
When invoking `uc_vm_free()` on a VM with enabled `.setup_signal_handlers`
configuration, reset system signal handlers back to their default actions.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

vm.c
@@ -185,6 +185,26 @@ uc_vm_signal_handlers_setup(uc_vm_t *vm)
 	tctx->signal_handler_vm = vm;
 }
 
+static void
+uc_vm_signal_handlers_reset(uc_vm_t *vm)
+{
+	uc_thread_context_t *tctx = uc_thread_context_get();
+	struct sigaction sa = { 0 };
+	size_t signo;
+
+	if (vm != tctx->signal_handler_vm)
+		return;
+
+	sa.sa_handler = SIG_DFL;
+	sigemptyset(&sa.sa_mask);
+
+	for (signo = 0; signo < ucv_array_length(vm->signal.handler); signo++)
+		if (ucv_is_callable(ucv_array_get(vm->signal.handler, signo)))
+			sigaction(signo, &sa, NULL);
+
+	tctx->signal_handler_vm = NULL;
+}
+
 void uc_vm_init(uc_vm_t *vm, uc_parse_config_t *config)
 {
 	vm->exception.type = EXCEPTION_NONE;
@@ -216,6 +236,8 @@ void uc_vm_free(uc_vm_t *vm)
 {
 	uc_upvalref_t *ref;
 	size_t i;
+
+	uc_vm_signal_handlers_reset(vm);
 
 	ucv_put(vm->exception.stacktrace);
 	free(vm->exception.message);

commit 0b80ceb29311b7eccce9d318e5315e5d88d29c28

vm: close signal pipe in uc_vm_signal_handlers_reset()
The previously introduced signal handler restoration logic did not take
the signal dispatching pipe into account.

Extend the `uc_vm_signal_handlers_reset()` function to also close any
related pipe handles.

Fixes: #255
Fixes: f9d2faf ("vm: reset signals when freeing VM")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

vm.c
@@ -190,7 +190,7 @@ uc_vm_signal_handlers_reset(uc_vm_t *vm)
 {
 	uc_thread_context_t *tctx = uc_thread_context_get();
 	struct sigaction sa = { 0 };
-	size_t signo;
+	size_t i, signo;
 
 	if (vm != tctx->signal_handler_vm)
 		return;
@@ -201,6 +201,13 @@ uc_vm_signal_handlers_reset(uc_vm_t *vm)
 	for (signo = 0; signo < ucv_array_length(vm->signal.handler); signo++)
 		if (ucv_is_callable(ucv_array_get(vm->signal.handler, signo)))
 			sigaction(signo, &sa, NULL);
+
+	for (i = 0; i < ARRAY_SIZE(vm->signal.sigpipe); i++) {
+		if (vm->signal.sigpipe[i] > STDERR_FILENO)
+			close(vm->signal.sigpipe[i]);
+
+		vm->signal.sigpipe[i] = -1;
+	}
 
 	tctx->signal_handler_vm = NULL;
 }