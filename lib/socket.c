jow-/ucode

commit fd202fd40bd1c3597f76b6e62f1781442b59ebd5

socket: respect port argument in sockinst.connect()
Implement the documented port argument for the socket instance `connect()`
function to allow passing the port number separately from the address
argument.

Fixes: #303
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

lib/socket.c
@@ -3285,15 +3285,29 @@ static uc_value_t *
 uc_socket_inst_connect(uc_vm_t *vm, size_t nargs)
 {
 	struct sockaddr_storage ss;
-	uc_value_t *addr;
+	uc_value_t *addr, *port;
+	unsigned long n;
 	int ret, sockfd;
 	socklen_t slen;
 
 	args_get(vm, nargs, &sockfd,
-		"address", UC_NULL, false, &addr);
+		"address", UC_NULL, false, &addr,
+		"port", UC_INTEGER, true, &port);
 
 	if (!uv_to_sockaddr(addr, &ss, &slen))
 		return NULL;
+
+	if (port) {
+		 if (ss.ss_family != AF_INET && ss.ss_family != AF_INET6)
+			err_return(EINVAL, "Port argument is only valid for IPv4 and IPv6 addresses");
+
+		n = ucv_to_unsigned(port);
+
+		if (errno != 0 || n > 65535)
+			err_return(EINVAL, "Invalid port number");
+
+		((struct sockaddr_in6 *)&ss)->sin6_port = htons(n);
+	}
 
 	ret = connect(sockfd, (struct sockaddr *)&ss, slen);
 

commit 37ac8f112af63e64c1ea2d13ae63c134b62a5681

socket: improve port argument validation in sockinst.connect()
Align port argument validation with other instances in the socket module.

Fixes: fd202fd ("socket: respect port argument in sockinst.connect()")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

lib/socket.c
@@ -3322,8 +3322,11 @@ uc_socket_inst_connect(uc_vm_t *vm, size_t nargs)
 
 		n = ucv_to_unsigned(port);
 
-		if (errno != 0 || n > 65535)
-			err_return(EINVAL, "Invalid port number");
+		if (n > 65535)
+			errno = ERANGE;
+
+		if (errno != 0)
+			err_return(errno, "Invalid port number");
 
 		((struct sockaddr_in6 *)&ss)->sin6_port = htons(n);
 	}