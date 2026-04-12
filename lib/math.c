jow-/ucode

commit ce0c8fc0ffea0d2b7d5cb453f9e46d9de2f8a150

docs: spelling fixes and usage notes
Add documentation fixes from PR #311.

- Fix typos in compiler.c, lexer.c, lib.c and lib modules
- Correct spelling: substract→subtract, encouter→encounter, elligible→eligible, etc.
- Add note section documenting ARGV and SCRIPT_NAME global variables
- Improve documentation formatting and readability

Signed-off-by: Paul Donald <newtwen+github@gmail.com>
[squashed commits, slightly rewrote notes section]
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

lib/math.c
@@ -123,15 +123,15 @@ uc_abs(uc_vm_t *vm, size_t nargs)
  *  - If `y` is +0 (-0) and `x` is -0, +pi (-pi) is returned.
  *  - If `y` is +0 (-0) and `x` is +0, +0 (-0) is returned.
  *  - If `y` is a finite value greater (less) than 0, and `x` is negative
- *    infinity, +pi (-pi) is returned.
+ *    iinfinity, +pi (-pi) is returned.
  *  - If `y` is a finite value greater (less) than 0, and `x` is positive
- *    infinity, +0 (-0) is returned.
- *  - If `y` is positive infinity (negative infinity), and `x` is finite,
+ *    iinfinity, +0 (-0) is returned.
+ *  - If `y` is positive iinfinity (negative iinfinity), and `x` is finite,
  *    pi/2 (-pi/2) is returned.
- *  - If `y` is positive infinity (negative infinity) and `x` is negative
- *    infinity, +3*pi/4 (-3*pi/4) is returned.
- *  - If `y` is positive infinity (negative infinity) and `x` is positive
- *    infinity, +pi/4 (-pi/4) is returned.
+ *  - If `y` is positive iinfinity (negative iinfinity) and `x` is negative
+ *    iinfinity, +3*pi/4 (-3*pi/4) is returned.
+ *  - If `y` is positive iinfinity (negative iinfinity) and `x` is positive
+ *    iinfinity, +pi/4 (-pi/4) is returned.
  *
  * When either `x` or `y` can't be converted to a numeric value, `NaN` is
  * returned.
@@ -189,10 +189,10 @@ uc_cos(uc_vm_t *vm, size_t nargs)
  *
  * On success, returns the exponential value of `x`.
  *
- *  - If `x` is positive infinity, positive infinity is returned.
- *  - If `x` is negative infinity, `+0` is returned.
+ *  - If `x` is positive iinfinity, positive iinfinity is returned.
+ *  - If `x` is negative iinfinity, `+0` is returned.
  *  - If the result underflows, a range error occurs, and zero is returned.
- *  - If the result overflows, a range error occurs, and `Infinity` is returned.
+ *  - If the result overflows, a range error occurs, and `Iinfinity` is returned.
  *
  * Returns `NaN` if the `x` value can't be converted to a number.
  *
@@ -220,10 +220,10 @@ uc_exp(uc_vm_t *vm, size_t nargs)
  * On success, returns the natural logarithm of `x`.
  *
  *  - If `x` is `1`, the result is `+0`.
- *  - If `x` is positive nfinity, positive infinity is returned.
+ *  - If `x` is positive infinity, positive iinfinity is returned.
  *  - If `x` is zero, then a pole error occurs, and the function
- *    returns negative infinity.
- *  - If `x` is negative (including negative infinity), then a domain
+ *    returns negative iinfinity.
+ *  - If `x` is negative (including negative iinfinity), then a domain
  *    error occurs, and `NaN` is returned.
  *
  * Returns `NaN` if the `x` value can't be converted to a number.
@@ -231,7 +231,7 @@ uc_exp(uc_vm_t *vm, size_t nargs)
  * @function module:math#log
  *
  * @param {number} x
- * Value to calulate natural logarithm of.
+ * Value to calculate natural logarithm of.
  *
  * @returns {number}
  */
@@ -251,7 +251,7 @@ uc_log(uc_vm_t *vm, size_t nargs)
  *
  * Returns the resulting sine value.
  *
- *  - When `x` is positive or negative infinity, a domain error occurs
+ *  - When `x` is positive or negative iinfinity, a domain error occurs
  *    and `NaN` is returned.
  *
  * Returns `NaN` if the `x` value can't be converted to a number.
@@ -275,12 +275,12 @@ uc_sin(uc_vm_t *vm, size_t nargs)
 }
 
 /**
- * Calculates the nonnegative square root of `x`.
+ * Calculates the non-negative square root of `x`.
  *
  * Returns the resulting square root value.
  *
  *  - If `x` is `+0` (`-0`) then `+0` (`-0`) is returned.
- *  - If `x` is positive infinity, positive infinity is returned.
+ *  - If `x` is positive iinfinity, positive iinfinity is returned.
  *  - If `x` is less than `-0`, a domain error occurs, and `NaN` is returned.
  *
  * Returns `NaN` if the `x` value can't be converted to a number.
@@ -309,43 +309,43 @@ uc_sqrt(uc_vm_t *vm, size_t nargs)
  * On success, returns the value of `x` raised to the power of `y`.
  *
  *  - If the result overflows, a range error occurs, and the function
- *    returns `Infinity`.
+ *    returns `Iinfinity`.
  *  - If result underflows, and is not representable, a range error
  *    occurs, and `0.0` with the appropriate sign is returned.
  *  - If `x` is `+0` or `-0`, and `y` is an odd integer less than `0`,
- *    a pole error occurs `Infinity` is returned, with the same sign
+ *    a pole error occurs `Iinfinity` is returned, with the same sign
  *    as `x`.
  *  - If `x` is `+0` or `-0`, and `y` is less than `0` and not an odd
- *    integer, a pole error occurs and `Infinity` is returned.
+ *    integer, a pole error occurs and `Iinfinity` is returned.
  *  - If `x` is `+0` (`-0`), and `y` is an odd integer greater than `0`,
  *    the result is `+0` (`-0`).
  *  - If `x` is `0`, and `y` greater than `0` and not an odd integer,
  *    the result is `+0`.
- *  - If `x` is `-1`, and `y` is positive infinity or negative infinity,
+ *  - If `x` is `-1`, and `y` is positive iinfinity or negative iinfinity,
  *    the result is `1.0`.
  *  - If `x` is `+1`, the result is `1.0` (even if `y` is `NaN`).
  *  - If `y` is `0`, the result is `1.0` (even if `x` is `NaN`).
  *  - If `x` is a finite value less than `0`, and `y` is a finite
- *    noninteger, a domain error occurs, and `NaN` is returned.
+ *    non-integer, a domain error occurs, and `NaN` is returned.
  *  - If the absolute value of `x` is less than `1`, and `y` is negative
- *    infinity, the result is positive infinity.
+ *    iinfinity, the result is positive iinfinity.
  *  - If the absolute value of `x` is greater than `1`, and `y` is
- *    negative infinity, the result is `+0`.
+ *    negative iinfinity, the result is `+0`.
  *  - If the absolute value of `x` is less than `1`, and `y` is positive
- *    infinity, the result is `+0`.
+ *    iinfinity, the result is `+0`.
  *  - If the absolute value of `x` is greater than `1`, and `y` is positive
- *    infinity, the result is positive infinity.
- *  - If `x` is negative infinity, and `y` is an odd integer less than `0`,
+ *    iinfinity, the result is positive iinfinity.
+ *  - If `x` is negative iinfinity, and `y` is an odd integer less than `0`,
  *    the result is `-0`.
- *  - If `x` is negative infinity, and `y` less than `0` and not an odd
+ *  - If `x` is negative iinfinity, and `y` less than `0` and not an odd
  *    integer, the result is `+0`.
- *  - If `x` is negative infinity, and `y` is an odd integer greater than
- *    `0`, the result is negative infinity.
- *  - If `x` is negative infinity, and `y` greater than `0` and not an odd
- *    integer, the result is positive infinity.
- *  - If `x` is positive infinity, and `y` less than `0`, the result is `+0`.
- *  - If `x` is positive infinity, and `y` greater than `0`, the result is
- *    positive infinity.
+ *  - If `x` is negative iinfinity, and `y` is an odd integer greater than
+ *    `0`, the result is negative iinfinity.
+ *  - If `x` is negative iinfinity, and `y` greater than `0` and not an odd
+ *    integer, the result is positive iinfinity.
+ *  - If `x` is positive iinfinity, and `y` less than `0`, the result is `+0`.
+ *  - If `x` is positive iinfinity, and `y` greater than `0`, the result is
+ *    positive iinfinity.
  *
  * Returns `NaN` if either the `x` or `y` value can't be converted to a number.
  *

compiler.c
@@ -1137,7 +1137,7 @@ uc_compiler_emit_inc_dec(uc_compiler_t *compiler, uc_tokentype_t toktype, bool i
 
 	insn = (toktype == TK_INC) ? I_PLUS : I_MINUS;
 
-	/* add / substract 1 */
+	/* add / subtract 1 */
 	uc_compiler_emit_insn(compiler, 0, I_LOAD8);
 	uc_compiler_emit_u8(compiler, 0, 1);
 
@@ -1167,7 +1167,7 @@ uc_compiler_emit_inc_dec(uc_compiler_t *compiler, uc_tokentype_t toktype, bool i
 		break;
 	}
 
-	/* for post increment or decrement, add/substract 1 to yield final value */
+	/* for post increment or decrement, add/subtract 1 to yield final value */
 	if (is_postfix) {
 		uc_compiler_emit_insn(compiler, 0, I_LOAD8);
 		uc_compiler_emit_u8(compiler, 0, 1);
@@ -1618,7 +1618,7 @@ uc_compiler_compile_paren(uc_compiler_t *compiler)
 				uc_compiler_parse_advance(compiler);
 			}
 
-			/* If we encouter a dot, treat potential subsequent keyword as label */
+			/* If we encounter a dot, treat potential subsequent keyword as label */
 			if (uc_compiler_parse_check(compiler, TK_DOT) ||
 			    uc_compiler_parse_check(compiler, TK_QDOT))
 				compiler->parser->lex.no_keyword = true;
@@ -1627,7 +1627,7 @@ uc_compiler_compile_paren(uc_compiler_t *compiler)
 		}
 	}
 
-	/* The lhs we parsed so far is elligible for an arrow function arg list,
+	/* The lhs we parsed so far is eligible for an arrow function arg list,
 	 * try to continue compiling into arrow function... */
 	if (maybe_arrowfn) {
 		/* If we can parse the remainder as arrow function, we're done */
@@ -2233,7 +2233,7 @@ uc_compiler_compile_object(uc_compiler_t *compiler)
 			/* parse property name expression */
 			uc_compiler_parse_precedence(compiler, P_ASSIGN);
 
-			/* cosume closing bracket and colon */
+			/* consume closing bracket and colon */
 			uc_compiler_parse_consume(compiler, TK_RBRACK);
 			uc_compiler_parse_consume(compiler, TK_COLON);
 
@@ -2932,7 +2932,7 @@ uc_compiler_compile_switch(uc_compiler_t *compiler)
 			uc_compiler_compile_declaration(compiler);
 		}
 
-		/* a statement or expression preceeding any `default` or `case` is a
+		/* a statement or expression preceding any `default` or `case` is a
 		 * syntax error */
 		else {
 			uc_compiler_syntax_error(compiler, compiler->parser->curr.pos,
@@ -3536,7 +3536,7 @@ uc_compiler_compile_module_source(uc_compiler_t *compiler, const char *modname, 
 	if (n_imports > 0xfffe)
 		uc_compiler_syntax_error(compiler, 0, "Too many imports");
 
-	/* emit non-wilcard import instructions */
+	/* emit non-wildcard import instructions */
 	for (i = 0; i < ucv_array_length(imports); i++) {
 		import = ucv_array_get(imports, i);
 

lexer.c
@@ -1296,7 +1296,7 @@ uc_lexer_is_keyword(uc_value_t *label)
 
 /*
  * Stores the given codepoint as a utf8 multibyte sequence into the given
- * output buffer and substracts the required amount of bytes from  the given
+ * output buffer and subtracts the required amount of bytes from the given
  * length pointer.
  *
  * Returns false if the multibyte sequence would not fit into the buffer,

lib.c
@@ -3498,7 +3498,7 @@ uc_json_from_object(uc_vm_t *vm, uc_value_t *obj, json_object **jso)
 		if (eof && !trail) {
 			ucv_put(rbuf);
 
-			/* Didn't parse a complete object yet, possibly a non-delimitted atomic value
+			/* Didn't parse a complete object yet, possibly a non-delimited atomic value
 			   such as `null`, `true` etc. - nudge parser by sending final zero byte.
 			   See json-c issue #681 <https://github.com/json-c/json-c/issues/681> */
 			if (json_tokener_get_error(tok) == json_tokener_continue)
@@ -4228,7 +4228,7 @@ uc_sleep(uc_vm_t *vm, size_t nargs)
 
 /**
  * Raise an exception with the given message parameter when the value in `cond`
- * is not truish.
+ * is not truthy.
  *
  * When `message` is omitted, the default value is `Assertion failed`.
  *
@@ -5181,7 +5181,7 @@ uc_timegm(uc_vm_t *vm, size_t nargs)
  * Reads the current second and microsecond value of the system clock.
  *
  * By default, the realtime clock is queried which might skew forwards or
- * backwards due to NTP changes, system sleep modes etc. If a truish value is
+ * backwards due to NTP changes, system sleep modes etc. If a truthy value is
  * passed as argument, the monotonic system clock is queried instead, which will
  * return the monotonically increasing time since some arbitrary point in the
  * past (usually the system boot time).

lib/debug.c
@@ -1235,7 +1235,7 @@ uc_getinfo(uc_vm_t *vm, size_t nargs)
  * The source line number where the local variable goes out of scope.
  *
  * @property {number} byteto
- * The source line offset where the local vatiable goes out of scope.
+ * The source line offset where the local variable goes out of scope.
  */
 static uc_value_t *
 uc_xlocal(uc_vm_t *vm, uc_value_t *level, uc_value_t *var, uc_value_t **set)

lib/fs.c
@@ -1869,7 +1869,7 @@ uc_fs_statvfs(uc_vm_t *vm, size_t nargs)
  *
  * Returns `true` if the directory was successfully created.
  *
- * Returns `null` if an error occurred, e.g. due to inexistent path.
+ * Returns `null` if an error occurred, e.g. due to non-existent path.
  *
  * @function module:fs#mkdir
  *
@@ -1903,7 +1903,7 @@ uc_fs_mkdir(uc_vm_t *vm, size_t nargs)
  *
  * Returns `true` if the directory was successfully removed.
  *
- * Returns `null` if an error occurred, e.g. due to inexistent path.
+ * Returns `null` if an error occurred, e.g. due to non-existent path.
  *
  * @function module:fs#rmdir
  *
@@ -1935,7 +1935,7 @@ uc_fs_rmdir(uc_vm_t *vm, size_t nargs)
  *
  * Returns `true` if the symlink was successfully created.
  *
- * Returns `null` if an error occurred, e.g. due to inexistent path.
+ * Returns `null` if an error occurred, e.g. due to non-existent path.
  *
  * @function module:fs#symlink
  *
@@ -1972,7 +1972,7 @@ uc_fs_symlink(uc_vm_t *vm, size_t nargs)
  *
  * Returns `true` if the unlink operation was successful.
  *
- * Returns `null` if an error occurred, e.g. due to inexistent path.
+ * Returns `null` if an error occurred, e.g. due to non-existent path.
  *
  * @function module:fs#unlink
  *

lib/log.c
@@ -619,8 +619,8 @@ parse_channels(uc_value_t *channels)
 /**
  * Configure ulog logger.
  *
- * This functions configures the ulog mechanism and is analogeous to using the
- * `openlog()` function in conjuncton with `syslog()`.
+ * This functions configures the ulog mechanism and is analogous to using the
+ * `openlog()` function in conjunction with `syslog()`.
  *
  * The `ulog_open()` function is OpenWrt specific and may not be present on
  * other systems. Use `openlog()` and `syslog()` instead for portability to
@@ -746,7 +746,7 @@ uc_ulog_log_common(uc_vm_t *vm, size_t nargs, int priority)
  *
  * If the `ulog_open()` function has not been called explicitly before, `ulog()`
  * implicitly configures certain defaults, see
- * {@link module:log#ulog_open|ulog_open()} for a detailled description.
+ * {@link module:log#ulog_open|ulog_open()} for a detailed description.
  *
  * If the `format` argument is not a string and not `null`, it will be
  * implicitly converted to a string and logged as-is, without further format
@@ -865,7 +865,7 @@ uc_ulog_close(uc_vm_t *vm, size_t nargs)
  * // Set threshold to "warning" or more severe
  * ulog_threshold(LOG_WARNING);
  *
- * // This message will be supressed
+ * // This message will be suppressed
  * ulog(LOG_DEBUG, "Testing thresholds");
  *
  * // Using priority name

commit 3c9d3b5272d7bf28b2a62beddc4dda0516262b98

docs: math: fix spelling mistakes in documentation
A previous attempt to fix wrong `nfinity` spelling over-matched, breaking
other instances.

Fixes: ce0c8fc ("docs: spelling fixes and usage notes")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

lib/math.c
@@ -123,15 +123,15 @@ uc_abs(uc_vm_t *vm, size_t nargs)
  *  - If `y` is +0 (-0) and `x` is -0, +pi (-pi) is returned.
  *  - If `y` is +0 (-0) and `x` is +0, +0 (-0) is returned.
  *  - If `y` is a finite value greater (less) than 0, and `x` is negative
- *    iinfinity, +pi (-pi) is returned.
+ *    infinity, +pi (-pi) is returned.
  *  - If `y` is a finite value greater (less) than 0, and `x` is positive
- *    iinfinity, +0 (-0) is returned.
- *  - If `y` is positive iinfinity (negative iinfinity), and `x` is finite,
+ *    infinity, +0 (-0) is returned.
+ *  - If `y` is positive infinity (negative infinity), and `x` is finite,
  *    pi/2 (-pi/2) is returned.
- *  - If `y` is positive iinfinity (negative iinfinity) and `x` is negative
- *    iinfinity, +3*pi/4 (-3*pi/4) is returned.
- *  - If `y` is positive iinfinity (negative iinfinity) and `x` is positive
- *    iinfinity, +pi/4 (-pi/4) is returned.
+ *  - If `y` is positive infinity (negative infinity) and `x` is negative
+ *    infinity, +3*pi/4 (-3*pi/4) is returned.
+ *  - If `y` is positive infinity (negative infinity) and `x` is positive
+ *    infinity, +pi/4 (-pi/4) is returned.
  *
  * When either `x` or `y` can't be converted to a numeric value, `NaN` is
  * returned.
@@ -189,10 +189,10 @@ uc_cos(uc_vm_t *vm, size_t nargs)
  *
  * On success, returns the exponential value of `x`.
  *
- *  - If `x` is positive iinfinity, positive iinfinity is returned.
- *  - If `x` is negative iinfinity, `+0` is returned.
+ *  - If `x` is positive infinity, positive infinity is returned.
+ *  - If `x` is negative infinity, `+0` is returned.
  *  - If the result underflows, a range error occurs, and zero is returned.
- *  - If the result overflows, a range error occurs, and `Iinfinity` is returned.
+ *  - If the result overflows, a range error occurs, and `Infinity` is returned.
  *
  * Returns `NaN` if the `x` value can't be converted to a number.
  *
@@ -220,10 +220,10 @@ uc_exp(uc_vm_t *vm, size_t nargs)
  * On success, returns the natural logarithm of `x`.
  *
  *  - If `x` is `1`, the result is `+0`.
- *  - If `x` is positive infinity, positive iinfinity is returned.
+ *  - If `x` is positive infinity, positive infinity is returned.
  *  - If `x` is zero, then a pole error occurs, and the function
- *    returns negative iinfinity.
- *  - If `x` is negative (including negative iinfinity), then a domain
+ *    returns negative infinity.
+ *  - If `x` is negative (including negative infinity), then a domain
  *    error occurs, and `NaN` is returned.
  *
  * Returns `NaN` if the `x` value can't be converted to a number.
@@ -251,7 +251,7 @@ uc_log(uc_vm_t *vm, size_t nargs)
  *
  * Returns the resulting sine value.
  *
- *  - When `x` is positive or negative iinfinity, a domain error occurs
+ *  - When `x` is positive or negative infinity, a domain error occurs
  *    and `NaN` is returned.
  *
  * Returns `NaN` if the `x` value can't be converted to a number.
@@ -280,7 +280,7 @@ uc_sin(uc_vm_t *vm, size_t nargs)
  * Returns the resulting square root value.
  *
  *  - If `x` is `+0` (`-0`) then `+0` (`-0`) is returned.
- *  - If `x` is positive iinfinity, positive iinfinity is returned.
+ *  - If `x` is positive infinity, positive infinity is returned.
  *  - If `x` is less than `-0`, a domain error occurs, and `NaN` is returned.
  *
  * Returns `NaN` if the `x` value can't be converted to a number.
@@ -309,43 +309,43 @@ uc_sqrt(uc_vm_t *vm, size_t nargs)
  * On success, returns the value of `x` raised to the power of `y`.
  *
  *  - If the result overflows, a range error occurs, and the function
- *    returns `Iinfinity`.
+ *    returns `Infinity`.
  *  - If result underflows, and is not representable, a range error
  *    occurs, and `0.0` with the appropriate sign is returned.
  *  - If `x` is `+0` or `-0`, and `y` is an odd integer less than `0`,
- *    a pole error occurs `Iinfinity` is returned, with the same sign
+ *    a pole error occurs `Infinity` is returned, with the same sign
  *    as `x`.
  *  - If `x` is `+0` or `-0`, and `y` is less than `0` and not an odd
- *    integer, a pole error occurs and `Iinfinity` is returned.
+ *    integer, a pole error occurs and `Infinity` is returned.
  *  - If `x` is `+0` (`-0`), and `y` is an odd integer greater than `0`,
  *    the result is `+0` (`-0`).
  *  - If `x` is `0`, and `y` greater than `0` and not an odd integer,
  *    the result is `+0`.
- *  - If `x` is `-1`, and `y` is positive iinfinity or negative iinfinity,
+ *  - If `x` is `-1`, and `y` is positive infinity or negative infinity,
  *    the result is `1.0`.
  *  - If `x` is `+1`, the result is `1.0` (even if `y` is `NaN`).
  *  - If `y` is `0`, the result is `1.0` (even if `x` is `NaN`).
  *  - If `x` is a finite value less than `0`, and `y` is a finite
  *    non-integer, a domain error occurs, and `NaN` is returned.
  *  - If the absolute value of `x` is less than `1`, and `y` is negative
- *    iinfinity, the result is positive iinfinity.
+ *    infinity, the result is positive infinity.
  *  - If the absolute value of `x` is greater than `1`, and `y` is
- *    negative iinfinity, the result is `+0`.
+ *    negative infinity, the result is `+0`.
  *  - If the absolute value of `x` is less than `1`, and `y` is positive
- *    iinfinity, the result is `+0`.
+ *    infinity, the result is `+0`.
  *  - If the absolute value of `x` is greater than `1`, and `y` is positive
- *    iinfinity, the result is positive iinfinity.
- *  - If `x` is negative iinfinity, and `y` is an odd integer less than `0`,
+ *    infinity, the result is positive infinity.
+ *  - If `x` is negative infinity, and `y` is an odd integer less than `0`,
  *    the result is `-0`.
- *  - If `x` is negative iinfinity, and `y` less than `0` and not an odd
+ *  - If `x` is negative infinity, and `y` less than `0` and not an odd
  *    integer, the result is `+0`.
- *  - If `x` is negative iinfinity, and `y` is an odd integer greater than
- *    `0`, the result is negative iinfinity.
- *  - If `x` is negative iinfinity, and `y` greater than `0` and not an odd
- *    integer, the result is positive iinfinity.
- *  - If `x` is positive iinfinity, and `y` less than `0`, the result is `+0`.
- *  - If `x` is positive iinfinity, and `y` greater than `0`, the result is
- *    positive iinfinity.
+ *  - If `x` is negative infinity, and `y` is an odd integer greater than
+ *    `0`, the result is negative infinity.
+ *  - If `x` is negative infinity, and `y` greater than `0` and not an odd
+ *    integer, the result is positive infinity.
+ *  - If `x` is positive infinity, and `y` less than `0`, the result is `+0`.
+ *  - If `x` is positive infinity, and `y` greater than `0`, the result is
+ *    positive infinity.
  *
  * Returns `NaN` if either the `x` or `y` value can't be converted to a number.
  *